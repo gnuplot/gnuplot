39e6e0cc6bac6b8a3f40828f5f5e99a199627fb2
Lars Hecking <lhecking@nmrc.ucc.ie>
1998-04-15 20:51:24 +0100

    Use AMIGA for Amiga compilers, move X11 term inclusion.

--- term.h
+++ term.h
@@ -1,56 +1,40 @@
 
 /*
- * $Id: term.h,v 1.41 1997/07/22 23:20:50 drd Exp $
+ * $Id: term.h,v 1.43 1998/03/22 23:31:28 drd Exp $
  *
  */
 
 /* GNUPLOT - term.h */
-/*
- * Copyright (C) 1986 - 1993, 1997   Thomas Williams, Colin Kelley
+
+/*[
+ * Copyright 1986 - 1993, 1998   Thomas Williams, Colin Kelley
  *
  * Permission to use, copy, and distribute this software and its
- * documentation for any purpose with or without fee is hereby granted, 
- * provided that the above copyright notice appear in all copies and 
- * that both that copyright notice and this permission notice appear 
+ * documentation for any purpose with or without fee is hereby granted,
+ * provided that the above copyright notice appear in all copies and
+ * that both that copyright notice and this permission notice appear
  * in supporting documentation.
  *
  * Permission to modify the software is granted, but not the right to
- * distribute the modified code.  Modifications are to be distributed 
- * as patches to released version.
- *  
- * This software is provided "as is" without express or implied warranty.
- * 
+ * distribute the complete modified source code.  Modifications are to
+ * be distributed as patches to the released version.  Permission to
+ * distribute binaries produced by compiling modified sources is granted,
+ * provided you
+ *   1. distribute the corresponding source modifications from the
+ *    released version in the form of a patch file along with the binaries,
+ *   2. add special version identification to distinguish your version
+ *    in addition to the base release version number,
+ *   3. provide your name and address as the primary contact for the
+ *    support of your modified version, and
+ *   4. retain our contact information in regard to use of the base
+ *    software.
+ * Permission to distribute the released version of the source code along
+ * with corresponding source modifications in the form of a patch file is
+ * granted with same provisions 2 through 4 for binary distributions.
  *
- * AUTHORS
- * 
- *   Original Software:
- *     Thomas Williams,  Colin Kelley.
- * 
- *   Gnuplot 2.0 additions:
- *       Russell Lang, Dave Kotz, John Campbell.
- *
- *   Gnuplot 3.0 additions:
- *       Gershon Elber and many others.
- * 
- * There is a mailing list for gnuplot users. Note, however, that the
- * newsgroup 
- *	comp.graphics.apps.gnuplot 
- * is identical to the mailing list (they
- * both carry the same set of messages). We prefer that you read the
- * messages through that newsgroup, to subscribing to the mailing list.
- * (If you can read that newsgroup, and are already on the mailing list,
- * please send a message to majordomo@dartmouth.edu, asking to be
- * removed from the mailing list.)
- *
- * The address for mailing to list members is
- *	   info-gnuplot@dartmouth.edu
- * and for mailing administrative requests is 
- *	   majordomo@dartmouth.edu
- * The mailing list for bug reports is 
- *	   bug-gnuplot@dartmouth.edu
- * The list of those interested in beta-test versions is
- *	   info-gnuplot-beta@dartmouth.edu
- */
+ * This software is provided "as is" without express or implied warranty
+ * to the extent permitted by applicable law.
+]*/
 
 /*
  * term.h: terminal support definitions
@@ -69,7 +53,7 @@
 
 /* Platform dependent part */
 
-#if defined(AMIGA_SC_6_1) || defined(AMIGA_AC_5) || defined(__amigaos__)
+#ifdef AMIGA
 #include "amiga.trm"
 #endif
 
@@ -111,7 +95,7 @@
 #ifndef _Windows     
 #ifdef PC
 /* uncomment the next line to include SuperVGA support */
-/* #define BGI_NAME "svga256" /* the name of the SVGA.BGI for Borland C */
+#define BGI_NAME "svga256" /* the name of the SVGA.BGI for Borland C */
 /* this also triggers the inclusion of Super VGA support */
 #include "pc.trm"          /* all PC types except MS WINDOWS*/
 #endif
@@ -151,7 +135,7 @@
 #include "mac.trm"
 #endif
 /* These terminals are not relevant for MSDOS, OS2, MS-Windows, ATARI or Amiga */
-#if !defined(MSDOS) && !defined(_Windows) && !defined(ATARI) && !defined(MTOS) && !defined(AMIGA_SC_6_1) && !defined(AMIGA_AC_5) && !defined(__amigaos__)
+#if !defined(MSDOS) && !defined(_Windows) && !defined(ATARI) && !defined(MTOS) && !defined(AMIGA)
 #include "aed.trm"     /* AED 512 and AED 767 */
 #ifdef UNIXPLOT
 #ifdef GNUGRAPH
@@ -167,24 +151,25 @@
                        /* Redwood Graphics Interface Protocol UNIPLEX */ 
 #include "t410x.trm"   /* Tektronix 4106, 4107, 4109 and 420x terminals */
 #include "tek.trm"     /* a Tek 4010 and others including VT-style */
-#ifdef X11
-#include "x11.trm"     /* x windows */
-#endif
 
 #include "xlib.trm"
 #endif /* !MSDOS && !OS2 && !_Windows && !_ATARI && !_MTOS && !AMIGA */
 
+#ifdef X11
+#include "x11.trm"     /* x windows */
+#endif
+
 /* These terminals can be used on any system */
 
-#include "ai.trm"	    /* Adobe Illustrator Format */
-#include "cgm.trm"      /* Computer Graphics Metafile (eg ms office) */
-#include "corel.trm"    /* CorelDraw! eps format */
-/* #include "debug.trm" /* debugging terminal */
-#include "dumb.trm"     /* dumb terminal */
-#include "dxf.trm"	    /* DXF format for use with AutoCad (Release 10.x) */
-#include "dxy.trm"      /* Roland DXY800A plotter */
-#include "excl.trm"	    /* QMS/EXCL laserprinter (Talaris 1590 and others) */
-#include "fig.trm"      /* fig graphics */
+/* #include "ai.trm"           /* Adobe Illustrator Format */
+/* #include "cgm.trm"      /* Computer Graphics Metafile (eg ms office) */
+/* #include "corel.trm"    /* CorelDraw! eps format */
+#include "debug.trm" /* debugging terminal */
+/* #include "dumb.trm"     /* dumb terminal */
+/* #include "dxf.trm"          /* DXF format for use with AutoCad (Release 10.x) */
+/* #include "dxy.trm"      /* Roland DXY800A plotter */
+/* #include "excl.trm"         /* QMS/EXCL laserprinter (Talaris 1590 and others) */
+/* #include "fig.trm"      /* fig graphics */
 
 /* NOTE THAT GIF REQUIRES A SEPARATE LIBRARY : see term/gif.trm */
 #ifdef HAVE_LIBGD /* autoconf */
@@ -192,15 +177,15 @@
 #endif
 
 /* #include "grass.trm" /* geographical info system */
-#include "hp26.trm"     /* HP2623A and probably others */
-#include "hp2648.trm"   /* HP2647 and 2648 */
-#include "hp500c.trm"   /* HP DeskJet 500 C */
-#include "hpgl.trm"	    /* HP7475, HP7220 plotters, and (hopefully) lots of others */
-#include "hpljii.trm"   /* HP Laserjet II */
-#include "hppj.trm"     /* HP PrintJet */
-#include "imagen.trm"   /* Imagen laser printers */
-#include "kyo.trm"      /* Kyocera Prescribe printer */
-#include "mif.trm"      /* Frame Maker MIF 3.00 format driver */
+/* #include "hp26.trm"     /* HP2623A and probably others */
+/* #include "hp2648.trm"   /* HP2647 and 2648 */
+/* #include "hp500c.trm"   /* HP DeskJet 500 C */
+/* #include "hpgl.trm"         /* HP7475, HP7220 plotters, and (hopefully) lots of others */
+/* #include "hpljii.trm"   /* HP Laserjet II */
+/* #include "hppj.trm"     /* HP PrintJet */
+/* #include "imagen.trm"   /* Imagen laser printers */
+/* #include "kyo.trm"      /* Kyocera Prescribe printer */
+/* #include "mif.trm"      /* Frame Maker MIF 3.00 format driver */
 #include "pbm.trm"      /* portable bit map */
 
 /* NOTE THAT PNG REQUIRES A SEPARATE LIBRARY : see term/png.trm */
@@ -208,12 +193,12 @@
 #include "png.trm"   /* png */
 #endif
 
-#include "post.trm"     /* postscript */
-#include "qms.trm"      /* QMS laser printers */
+/* #include "post.trm"     /* postscript */
+/* #include "qms.trm"      /* QMS laser printers */
 #include "table.trm"    /* built-in, but used for the documentation */
-#include "tgif.trm"     /* x11 tgif tool */
-#include "tkcanvas.trm" /* tcl/tk */
-#include "v384.trm"     /* Vectrix 384 printer, also Tandy colour */
+/* #include "tgif.trm"     /* x11 tgif tool */
+/* #include "tkcanvas.trm" /* tcl/tk */
+/* #include "v384.trm"     /* Vectrix 384 printer, also Tandy colour */
 
 /* wire printers */
 #define EPSONP          /* Epson LX-800, Star NL-10, NX-1000 and lots of others */
@@ -223,15 +208,15 @@
 #define OKIDATA
 #define STARC
 #define TANDY60         /* Tandy DMP-130 series 60-dot per inch graphics */
-#include "epson.trm"
+/* #include "epson.trm" /* the common driver file for all of these */
 
 /* TeX related terminals */
 #define EMTEX
-#include "latex.trm"    /* latex and emtex */
-#include "pslatex.trm"  /* latex/tex with picture in postscript */
-#include "eepic.trm"    /* EEPIC-extended LaTeX driver, for EEPIC users */
-#include "tpic.trm"     /* TPIC specials for TeX */
-#include "pstricks.trm" /* LaTeX picture environment with PSTricks macros */
-#include "texdraw.trm"  /* TeXDraw drawing package for LaTeX */
-#include "metafont.trm" /* METAFONT */
+/* #include "latex.trm"    /* latex and emtex */
+/* #include "pslatex.trm"  /* latex/tex with picture in postscript */
+/* #include "eepic.trm"    /* EEPIC-extended LaTeX driver, for EEPIC users */
+/* #include "tpic.trm"     /* TPIC specials for TeX */
+/* #include "pstricks.trm" /* LaTeX picture environment with PSTricks macros */
+/* #include "texdraw.trm"  /* TeXDraw drawing package for LaTeX */
+/* #include "metafont.trm" /* METAFONT */
 
