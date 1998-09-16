adebf509aa7bf0512d2bbf5bd489cbecf08f02b2
Lars Hecking <lhecking@nmrc.ucc.ie>
1998-09-16 12:33:01 +0100

    Alexander Mai's stat(2) patch.

--- datafile.c
+++ datafile.c
@@ -728,14 +728,26 @@ int max_using;
 		data_fp=lf_top();
 		if(!data_fp) data_fp=stdin;
 		mixed_data_fp=TRUE; /* don't close command file */
-	} else
+	} else {
+	        char msg[MAX_LINE_LEN];
+#ifndef NO_SYS_STAT_H
+		struct stat statbuf;
+
+		if (stat(filename,&statbuf) < 0) {
+		        fprintf(stderr,"(%s)\n\n", strerror(errno));
+		}
+                 else if (!(S_ISREG(statbuf.st_mode) || S_ISFIFO(statbuf.st_mode))) {
+                        sprintf(msg, "data file \"%s\" not a regular file or pipe", filename);
+                        os_error(msg, name_token);
+                }
+#endif /* NO_SYS_STAT_H */
 		if ((data_fp = fopen(filename, df_binary ? "rb" : "r")) == (FILE *) NULL)
 		{
 			/* one day we will have proper printf-style error reporting fns */
-			char msg[160];
 			sprintf(msg, "can't read data file \"%s\"", filename);
 			os_error(msg, name_token);
 		}
+	}
 /*}}}*/
 
 	return df_no_use_specs;
