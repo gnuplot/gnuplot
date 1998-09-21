47b8fc1e761fe6cdd53898b46a45b165fac0c9b2
Lars Hecking <lhecking@nmrc.ucc.ie>
1998-09-21 22:06:16 +0100

    Init some vars.

--- help.c
+++ help.c
@@ -1,55 +1,38 @@
 #ifndef lint
-static char *RCSid = "$Id: help.c,v 1.27 1997/05/27 01:28:50 drd Exp $";
+static char *RCSid = "$Id: help.c,v 1.29 1998/04/14 00:15:36 drd Exp $";
 #endif
 
-
 /* GNUPLOT - help.c */
-/*
- * Copyright (C) 1986 - 1993, 1997   Thomas Williams, Colin Kelley
+
+/*[
+ * Copyright 1986 - 1993, 1998   Thomas Williams, Colin Kelley
  *
  * Permission to use, copy, and distribute this software and its
- * documentation for any purpose with or without fee is hereby granted, 
- * provided that the above copyright notice appear in all copies and 
- * that both that copyright notice and this permission notice appear 
+ * documentation for any purpose with or without fee is hereby granted,
+ * provided that the above copyright notice appear in all copies and
+ * that both that copyright notice and this permission notice appear
  * in supporting documentation.
  *
  * Permission to modify the software is granted, but not the right to
- * distribute the modified code.  Modifications are to be distributed 
- * as patches to released version.
- *  
- * This software is provided "as is" without express or implied warranty.
- * 
+ * distribute the complete modified source code.  Modifications are to
+ * be distributed as patches to the released version.  Permission to
+ * distribute binaries produced by compiling modified sources is granted,
+ * provided you
+ *   1. distribute the corresponding source modifications from the
+ *    released version in the form of a patch file along with the binaries,
+ *   2. add special version identification to distinguish your version
+ *    in addition to the base release version number,
+ *   3. provide your name and address as the primary contact for the
+ *    support of your modified version, and
+ *   4. retain our contact information in regard to use of the base
+ *    software.
+ * Permission to distribute the released version of the source code along
+ * with corresponding source modifications in the form of a patch file is
+ * granted with same provisions 2 through 4 for binary distributions.
  *
- * AUTHORS
- * 
- *   Original Software:
- *     Thomas Williams,  Colin Kelley.
- * 
- *   Gnuplot 2.0 additions:
- *       Russell Lang, Dave Kotz, John Campbell.
- *
- *   Gnuplot 3.0 additions:
- *       Gershon Elber and many others.
- * 
- * There is a mailing list for gnuplot users. Note, however, that the
- * newsgroup 
- *	 comp.graphics.apps.gnuplot 
- * is identical to the mailing list (they
- * both carry the same set of messages). We prefer that you read the
- * messages through that newsgroup, to subscribing to the mailing list.
- * (If you can read that newsgroup, and are already on the mailing list,
- * please send a message to majordomo@dartmouth.edu, asking to be
- * removed from the mailing list.)
- *
- * The address for mailing to list members is
- *	   info-gnuplot@dartmouth.edu
- * and for mailing administrative requests is 
- *	   majordomo@dartmouth.edu
- * The mailing list for bug reports is 
- *	   bug-gnuplot@dartmouth.edu
- * The list of those interested in beta-test versions is
- *	   info-gnuplot-beta@dartmouth.edu
- */
+ * This software is provided "as is" without express or implied warranty
+ * to the extent permitted by applicable law.
+]*/
 
 #include "plot.h"
 
@@ -60,7 +43,7 @@ static char *RCSid = "$Id: help.c,v 1.27 1997/05/27 01:28:50 drd Exp $";
 /* since there are only two external references, we won't include protos.h */
 
 void int_error __PROTO((char str[], int t_num));
-int instring __PROTO((char *str, char c));
+extern int instring __PROTO((char *str, char c));
 
 #if defined(__EMX__) || defined(DJGPP) || defined(DOS386)
 #ifdef MSDOS
@@ -251,7 +234,7 @@ LoadHelp(path)
 	char *path;
 {
     LINKEY *key;			/* this key */
-    long pos;				/* ftell location within help file */
+    long pos = 0;			/* ftell location within help file */
     char buf[BUFSIZ];		/* line from help file */
     LINEBUF *head;			/* head of text list  */
     LINEBUF *firsthead = NULL;
@@ -566,14 +549,14 @@ ShowSubtopics(key, subtopics)
     char line[BUFSIZ];		/* subtopic output line */
     char *start;			/* position of subname in key name */
     int sublen;			/* length of subname */
-    int pos;
+    int pos = 0;
     char *prev = NULL;		/* the last thing we put on the list */
 
     *line = '\0';
     len = strlen(key->key);
 
     for (subkey = key+1; subkey->key != NULL; subkey++) {
-		int spacelen, ispacelen;
+		int spacelen = 0, ispacelen = 0;
 	   if (strncmp(subkey->key, key->key, len) == 0) {
 		  /* find this subtopic name */
 		  start = subkey->key + len;
