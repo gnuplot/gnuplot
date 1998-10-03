8d2e3f27fb022ef4766b10569fee4b04f7a2ce34
Lars Hecking <lhecking@nmrc.ucc.ie>
1998-10-03 21:14:29 +0100

    Update from A. Mai.

--- os2/gnupmdrv.c
+++ os2/gnupmdrv.c
@@ -1,5 +1,5 @@
 #ifdef INCRCSDATA
-static char RCSid[]="$Id: gnupmdrv.c,v 1.9 1997/04/08 06:14:42 drd Exp $" ;
+static char RCSid[]="$Id: gnupmdrv.c,v 1.10 1998/03/22 22:34:23 drd Exp $" ;
 #endif
 
 /****************************************************************************
@@ -14,23 +14,40 @@ static char RCSid[]="$Id: gnupmdrv.c,v 1.9 1997/04/08 06:14:42 drd Exp $" ;
        
 ****************************************************************************/
 
-/*
- * PM driver for GNUPLOT
- * Copyright (C) 1992   Roger Fearick
+/* PM driver for GNUPLOT */
+
+/*[
+ * Copyright 1992, 1993, 1998   Roger Fearick
  *
  * Permission to use, copy, and distribute this software and its
- * documentation for any purpose with or without fee is hereby granted, 
- * provided that the above copyright notice appear in all copies and 
- * that both that copyright notice and this permission notice appear 
+ * documentation for any purpose with or without fee is hereby granted,
+ * provided that the above copyright notice appear in all copies and
+ * that both that copyright notice and this permission notice appear
  * in supporting documentation.
  *
  * Permission to modify the software is granted, but not the right to
- * distribute the modified code.  Modifications are to be distributed 
- * as patches to released version.
- *  
- * This software is provided "as is" without express or implied warranty.
- * 
+ * distribute the complete modified source code.  Modifications are to
+ * be distributed as patches to the released version.  Permission to
+ * distribute binaries produced by compiling modified sources is granted,
+ * provided you
+ *   1. distribute the corresponding source modifications from the
+ *    released version in the form of a patch file along with the binaries,
+ *   2. add special version identification to distinguish your version
+ *    in addition to the base release version number,
+ *   3. provide your name and address as the primary contact for the
+ *    support of your modified version, and
+ *   4. retain our contact information in regard to use of the base
+ *    software.
+ * Permission to distribute the released version of the source code along
+ * with corresponding source modifications in the form of a patch file is
+ * granted with same provisions 2 through 4 for binary distributions.
  *
+ * This software is provided "as is" without express or implied warranty
+ * to the extent permitted by applicable law.
+]*/
+
+
+/*
  * AUTHOR
  * 
  *   Gnuplot driver for OS/2:  Roger Fearick
@@ -103,6 +120,14 @@ int main ( int argc, char **argv )
     PFNWP        pfnOldFrameWndProc ;
     HWND         hwndHelp ;        
     BOOL         bPos ;
+
+    /* (am, 19981001)
+     * A subtile problem is fixed here:
+     * upon the first initialization of this driver (i.e. we're here in main())
+     * it may inherit handles of files opened (temporarily) by gnuplot itself!
+     * We close them here.
+     */
+    fcloseall();
 
     if( argc <= 1 ) strcpy( szIPCName, IPCDEFAULT ) ;
     else {
