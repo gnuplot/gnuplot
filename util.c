3f2b276bd25ef5fa1e1c6603d0d6c352b33715d7
Lars Hecking <lhecking@nmrc.ucc.ie>
1998-11-06 22:07:07 +0000

    Make second arg int instead of char.

--- util.c
+++ util.c
@@ -52,13 +52,9 @@ static void parse_esc __PROTO((char *instr));
  * chr_in_str() compares the characters in the string of token number t_num
  * with c, and returns TRUE if a match was found.
  */
-#ifdef PROTOTYPES
-int chr_in_str(int t_num, char c)
-#else
 int chr_in_str(t_num, c)
 int t_num;
-char c;
-#endif
+int c;
 {
     register int i;
 
