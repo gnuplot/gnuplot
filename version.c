9babd95a46d373fa0c5d8c39ed74864bbb358436
Lars Hecking <lhecking@nmrc.ucc.ie>
1998-11-21 18:07:16 +0000

    Patchlevel pl7 and date.

--- version.c
+++ version.c
@@ -37,8 +37,8 @@ static char *RCSid = "$Id: version.c,v 1.347 1998/06/22 12:24:56 ddenholm Exp $"
 #include "plot.h"
 
 char version[] = "3.5 (pre 3.6)";
-char patchlevel[] = "beta 347pl6";
-char date[] = "Mon Jun 22 13:22:33 BST 1998"; 
+char patchlevel[] = "beta 347pl7";
+char date[] = "Sat Nov 21 18:03:42 BST 1998"; 
 char gnuplot_copyright[] = "Copyright(C) 1986 - 1993, 1998";
 
 char faq_location[] = FAQ_LOCATION;
