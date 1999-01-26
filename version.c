90651529c0a1909791adf6d2d91e4b148ad8d996
Lars Hecking <lhecking@nmrc.ucc.ie>
1999-01-26 13:49:45 +0000

    New patchlevel.

--- version.c
+++ version.c
@@ -37,7 +37,7 @@ static char *RCSid = "$Id: version.c,v 1.347 1998/06/22 12:24:56 ddenholm Exp $"
 #include "plot.h"
 
 char version[] = "3.7";
-char patchlevel[] = "0";
+char patchlevel[] = "1";
 char date[] = "Thu Jan 14 19:34:53 BST 1999";
 char gnuplot_copyright[] = "Copyright(C) 1986 - 1993, 1998, 1999";
 
