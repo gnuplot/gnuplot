bf68b7e36b25fe55a65c282afefd63b2ea8d7266
Lars Hecking <lhecking@nmrc.ucc.ie>
1999-03-04 20:49:26 +0000

    Fix Be/NEXT init. string compare funcs.

--- term.c
+++ term.c
@@ -875,7 +875,7 @@ int length;
     struct termentry *t = NULL;
 
     for (i = 0; i < TERMCOUNT; i++) {
-	if (!strncmp(name, term_tbl[i].name, length)) {
+	if (STREQN(name, term_tbl[i].name, length)) {
 	    if (t)
 		return (NULL);	/* ambiguous */
 	    t = term_tbl + i;
@@ -911,7 +911,7 @@ int length;
 	term->linewidth = null_linewidth;
 
     /* Special handling for unixplot term type */
-    if (!strncmp("unixplot", name, 8)) {
+    if (STREQN("unixplot", name, 8)) {
 	UP_redirect(2);		/* Redirect actual stdout for unixplots */
     } else if (unixplot) {
 	UP_redirect(3);		/* Put stdout back together again. */
@@ -961,14 +961,14 @@ void init_terminal()
 #ifdef NEXT
 	env_term = getenv("TERM");
 	if (term_name == (char *) NULL
-	    && env_term != (char *) NULL && STREQ(term, "next"))
+	    && env_term != (char *) NULL && STREQ(env_term, "next"))
 	    term_name = "next";
 #endif /* NeXT */
 
 #ifdef __BEOS__
 	env_term = getenv("TERM");
 	if (term_name == (char *) NULL
-	    && env_term != (char *) NULL && STREQ(term, "beterm"))
+	    && env_term != (char *) NULL && STREQ(env_term, "beterm"))
 	    term_name = "be";
 #endif /* BeOS */
 
