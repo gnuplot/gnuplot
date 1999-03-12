5893bab8edb7a7a2bbff924d6781107786859a50
Lars Hecking <lhecking@nmrc.ucc.ie>
1999-03-12 21:47:31 +0000

    Patchlevel 0.3.

--- version.c
+++ version.c
@@ -37,8 +37,8 @@ static char *RCSid = "$Id: version.c,v 1.347 1998/06/22 12:24:56 ddenholm Exp $"
 #include "plot.h"
 
 char version[] = "3.7";
-char patchlevel[] = "0.2";
-char date[] = "Thu Mar 11 12:44:54 GMT 1999";
+char patchlevel[] = "0.3";
+char date[] = "Fri Mar 12 20:06:24 GMT 1999";
 char gnuplot_copyright[] = "Copyright(C) 1986 - 1993, 1999";
 
 char faq_location[] = FAQ_LOCATION;
