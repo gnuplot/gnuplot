01315c71fbd68f63f3741c7e6d6fc17bdeb7548e
Lars Hecking <lhecking@nmrc.ucc.ie>
1999-03-26 15:15:14 +0000

    Add info directory entry.

--- docs/doc2info.c
+++ docs/doc2info.c
@@ -148,7 +148,13 @@ FILE *a, *b;
     refhead->next = refhead->prev = NULL;
 
     /* Info header */
-    fprintf(b, "This file is %s created by doc2info from %s.\n\n", ofile, ifile);
+    fprintf(b, "\
+This file is %s created by doc2info from %s.\n\
+\n\
+START-INFO-DIR-ENTRY\n\
+* Gnuplot: (gnuplot).           Gnuplot plotting program\n\
+END-INFO-DIR-ENTRY\n\n",
+	    ofile, ifile);
 
     /* and Top node */
     node_head(NULL, NULL, head->next->string, NULL, b);
