b20b5096db2f52e6c3cb925bc035a198162a413a
Lars Hecking <lhecking@nmrc.ucc.ie>
1999-05-14 20:42:45 +0100

    Add is_notempty() prototype.

--- src/protos.h
+++ src/protos.h
@@ -287,6 +287,7 @@ int almost_equals __PROTO((int t_num, char *str));
 int isstring __PROTO((int t_num));
 int isanumber __PROTO((int t_num));
 int isletter __PROTO((int t_num));
+int is_notempty __PROTO((int t_num));
 int is_definition __PROTO((int t_num));
 void copy_str __PROTO((char str[], int t_num, int max));
 int token_len __PROTO((int t_num));
