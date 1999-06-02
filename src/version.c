521e37bfe329b10f6e93832546d2b0ea7e47839f
Lars Hecking <lhecking@nmrc.ucc.ie>
1999-06-02 18:06:37 +0100

    Regenerated.

--- src/version.c
+++ src/version.c
@@ -38,7 +38,7 @@ static char *RCSid = "$Id: version.c,v 1.347 1998/06/22 12:24:56 ddenholm Exp $"
 
 char version[] = "3.7";
 char patchlevel[] = "0.7";
-char date[] = "Mon May 31 18:24:28 BST 1999";
+char date[] = "Wed Jun  2 14:46:40 BST 1999";
 char gnuplot_copyright[] = "Copyright(C) 1986 - 1993, 1999";
 
 char faq_location[] = FAQ_LOCATION;
