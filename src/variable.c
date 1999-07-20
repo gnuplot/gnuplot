e9a1941d8a0825d762b2ac83bd905de682c6aa1a
Lars Hecking <lhecking@nmrc.ucc.ie>
1999-07-20 16:27:48 +0100

    Change no loadpath message.

--- src/variable.c
+++ src/variable.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: variable.c,v 1.7 1999/06/17 14:16:09 lhecking Exp $"); }
+static char *RCSid() { return RCSid("$Id: variable.c,v 1.8 1999/06/22 11:56:50 lhecking Exp $"); }
 #endif
 
 /* GNUPLOT - variable.c */
@@ -140,7 +140,7 @@ char *path;
 		PRINT_LOADPATH(last);
 	    }
 	} else
-	    fputs("\tno loadpath\n", stderr);
+	    fputs("\tloadpath is empty\n", stderr);
 	break;
     case ACTION_SAVE:
 	/* we don't save the load path taken from the
