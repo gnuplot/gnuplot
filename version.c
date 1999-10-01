7b5bb5a3664ce8cd7389bd8eb57581452f140337
Lars Hecking <lhecking@nmrc.ucc.ie>
1999-10-01 11:36:17 +0100

    Updated.

--- version.c
+++ version.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid = "$Id: version.c,v 1.22.2.17 1999/09/29 13:48:32 lhecking Exp $";
+static char *RCSid = "$Id: version.c,v 1.22.2.18 1999/09/29 17:17:39 lhecking Exp $";
 #endif
 
 /* GNUPLOT - version.c */
@@ -38,7 +38,7 @@ static char *RCSid = "$Id: version.c,v 1.22.2.17 1999/09/29 13:48:32 lhecking Ex
 
 char version[] = "3.7.1";
 char patchlevel[] = "beta8";
-char date[] = "Wed Sep 29 17:09:16 BST 1999";
+char date[] = "Fri Oct  1 11:31:07 BST 1999";
 char gnuplot_copyright[] = "Copyright(C) 1986 - 1993, 1998, 1999";
 
 char faq_location[] = FAQ_LOCATION;
