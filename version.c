1a41d363a265185585f44661241db7df3c7f95a3
Hans-Bernhard Broeker <broeker@physik.rwth-aachen.de>
1999-10-11 13:20:08 +0100

    Updated.

--- version.c
+++ version.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid = "$Id: version.c,v 1.22.2.20 1999/10/01 12:51:06 lhecking Exp $";
+static char *RCSid = "$Id: version.c,v 1.22.2.21 1999/10/01 18:21:44 lhecking Exp $";
 #endif
 
 /* GNUPLOT - version.c */
@@ -37,8 +37,8 @@ static char *RCSid = "$Id: version.c,v 1.22.2.20 1999/10/01 12:51:06 lhecking Ex
 #include "plot.h"
 
 char version[] = "3.7.1";
-char patchlevel[] = "beta8";
-char date[] = "Fri Oct  1 18:58:18 BST 1999";
+char patchlevel[] = "beta9";
+char date[] = "Mon Oct 11 11:59:56 BST 1999";
 char gnuplot_copyright[] = "Copyright(C) 1986 - 1993, 1998, 1999";
 
 char faq_location[] = FAQ_LOCATION;
