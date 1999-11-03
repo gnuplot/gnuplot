9b48def6980d16438aa9a7c344949959f568e9d0
Lars Hecking <lhecking@nmrc.ucc.ie>
1999-11-03 16:07:07 +0000

    Removed.

--- docs/fixinfo.el
+++
@@ -1,15 +0,0 @@
-;;; fixinfo.el --- fix gnuplot.info file.
-
-;; Author: Denis Kosygin <kosygin@cims.nyu.edu>
-;; Copyright: public domain.
-
-;;; Code:
-;(setq backup-inhibited)
-(find-file "gnuplot.info")
-(goto-char (point-min))
-(while (search-forward "" nil t)
-  (delete-backward-char 1))
-(Info-tagify)
-(save-buffer)
-;;; fixinfo.el ends here
-
