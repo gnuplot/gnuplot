adab0c384c774ff419b8f5329a6d373540c5a597
Lars Hecking <lhecking@nmrc.ucc.ie>
2000-05-02 19:16:44 +0100

    Add gnuplot copyright statement, RCD id.

--- src/ipc.h
+++ src/ipc.h
@@ -1,13 +1,47 @@
-/* -*- C -*-
- * FILE: "/home/joze/pub/gnuplot/src/ipc.h"
+/*
+ * $Id: ipc.h,v 1.2 2000/03/28 21:28:33 lhecking Exp $
+ */
+
+/* GNUPLOT - ipc.h */
+
+/*[
+ * Copyright 1999   Thomas Williams, Colin Kelley
+ *
+ * Permission to use, copy, and distribute this software and its
+ * documentation for any purpose with or without fee is hereby granted,
+ * provided that the above copyright notice appear in all copies and
+ * that both that copyright notice and this permission notice appear
+ * in supporting documentation.
+ *
+ * Permission to modify the software is granted, but not the right to
+ * distribute the complete modified source code.  Modifications are to
+ * be distributed as patches to the released version.  Permission to
+ * distribute binaries produced by compiling modified sources is granted,
+ * provided you
+ *   1. distribute the corresponding source modifications from the
+ *    released version in the form of a patch file along with the binaries,
+ *   2. add special version identification to distinguish your version
+ *    in addition to the base release version number,
+ *   3. provide your name and address as the primary contact for the
+ *    support of your modified version, and
+ *   4. retain our contact information in regard to use of the base
+ *    software.
+ * Permission to distribute the released version of the source code along
+ * with corresponding source modifications in the form of a patch file is
+ * granted with same provisions 2 through 4 for binary distributions.
+ *
+ * This software is provided "as is" without express or implied warranty
+ * to the extent permitted by applicable law.
+]*/
+
+/*
  * LAST MODIFICATION: "Wed, 22 Mar 2000 21:31:36 +0100 (joze)"
  * 1999 by Johannes Zellner, <johannes@zellner.org>
- * $Id: ipc.h,v 1.1 2000/02/11 18:41:55 lhecking Exp $
  */
 
 
 #ifndef _IPC_H
-#define _IPC_H
+# define _IPC_H
 
 char* readline_ipc __PROTO((const char*));
 /*
@@ -15,7 +49,7 @@ char* readline_ipc __PROTO((const char*));
  * otherwise (OS/2)
  */
 
-#ifndef OS2
+# ifndef OS2
 /*
  * gnuplot's terminals communicate with gnuplot by shared memory + event
  * semaphores, thus the code below is not used (see also gpexecute.inc)
@@ -30,14 +64,14 @@ enum { IPC_BACK_UNUSABLE = -2, IPC_BACK_CLOSED = -1 };
  * commands to gnuplot.  Note, that terminals using this fd
  * should set it to a negative value when closing. (joze)
  */
-#ifdef _TERM_C
+#  ifdef _TERM_C
     int ipc_back_fd = IPC_BACK_CLOSED;
     int isatty_state = 1;
-#else
+#  else
     extern int ipc_back_fd;
     extern int isatty_state;
-#endif
+#  endif
 
-#endif /* OS/2 */
+# endif /* OS/2 */
 
 #endif /* _IPC_H */
