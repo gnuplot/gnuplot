dbc97843a6a408702b96a2196b7527409b8e9a96
Lars Hecking <lhecking@nmrc.ucc.ie>
2000-05-02 19:53:53 +0100

    Add external variable declaration.

--- src/plot3d.h
+++ src/plot3d.h
@@ -1,6 +1,5 @@
 /*
- * $Id: plot3d.h,v 1.1 1999/10/01 14:04:22 lhecking Exp $
- *
+ * $Id: plot3d.h,v 1.2 1999/11/08 19:24:16 lhecking Exp $
  */
 
 /* GNUPLOT - plot3d.h */
@@ -43,6 +42,7 @@
 /* Variables of plot3d.c needed by other modules: */
 
 extern struct surface_points *first_3dplot;
+extern int plot3d_num;
 
 /* prototypes from plot3d.c */
 
