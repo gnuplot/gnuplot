e9e0b8334d3e0fde9f076e7bae79faf3a7c7fe56
Hans-Bernhard Broeker <broeker@physik.rwth-aachen.de>
2001-03-02 16:21:09 +0100

    Added PDF driver #include to term.h

--- src/term.h
+++ src/term.h
@@ -1,5 +1,5 @@
 /*
- * $Id: term.h,v 1.12 2000/12/18 08:21:17 mikulik Exp $
+ * $Id: term.h,v 1.13 2001/01/16 20:56:09 broeker Exp $
  */
 
 /* GNUPLOT - term.h */
@@ -328,6 +328,12 @@
 
 /* portable bit map */
 #include "pbm.trm"
+
+/* Adobe Portable Document Format (PDF) */
+/* NOTE THAT PDF REQUIRES A SEPARATE LIBRARY : see term/pdf.trm */
+#ifdef HAVE_LIBPDF
+# include "pdf.trm"
+#endif
 
 /* NOTE THAT PNG REQUIRES A SEPARATE LIBRARY : see term/png.trm */
 #ifdef HAVE_LIBPNG
