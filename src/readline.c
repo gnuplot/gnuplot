24322e54959eb4666fc3fb39daa269cec866c0a7
Johannes Zellner <johannes@zellner.org>
2002-01-23 11:16:53 +0000

    src/readline.c: switching to unbuffered input solved the problem...
    ...problem that escape sequences (key up) and mouse pasted text didn't
    get thru.

--- src/readline.c
+++ src/readline.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: readline.c,v 1.24 2001/09/18 23:08:26 amai Exp $"); }
+static char *RCSid() { return RCSid("$Id: readline.c,v 1.25 2001/09/28 13:38:37 amai Exp $"); }
 #endif
 
 /* GNUPLOT - readline.c */
@@ -83,7 +83,12 @@ getc_wrapper(FILE* fp /* should be stdin, supplied by readline */)
 {
 #ifdef USE_MOUSE
     if (term && term->waitforinput && interactive) {
-	return term->waitforinput();
+	int c;
+	/* fprintf(stderr, "(getc_wrapper) waiting ... "); */
+	/* fflush(stderr); */
+	c = term->waitforinput();
+	/* fprintf(stderr, " c = %d\n", c); */
+	return c;
     }
     else
 #endif
@@ -102,6 +107,13 @@ readline_ipc(const char* prompt)
 
     rl_callback_handler_install((char*) prompt, LineCompleteHandler);
     rl_getc_function = getc_wrapper;
+
+    /* apparently multy-character inputs like escape sequences
+     * but also mouse-pasted text got buffered and therefore
+     * didn't trigger the select() function in X11_waitforinput().
+     * Switching to unbuffered input solved this (although I don't
+     * really like this solution 23 Jan 2002 (joze) */
+    setvbuf(stdin, (char*)0, _IONBF, 0);
 
     for (line_complete = 0; !line_complete; /* EMPTY */) {
 	rl_callback_read_char(); /* stdin */
