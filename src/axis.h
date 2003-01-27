d14be238f4a58ebc1c6ae8321072e1d0db7a3cec
Petr Mikulik <mikulik@physics.muni.cz>
2003-01-27 10:30:28 +0100

    Removed patch 2003-01-25 about of the changed definition of AUTOSCALE_FIXMIN and AUTOSCALE_FIXMAX. The bug of reversed y-axis, "set autoscale fix" and mouse zooming seems to be more tricky.

--- src/axis.h
+++ src/axis.h
@@ -1,5 +1,5 @@
 /* 
- * $Id: axis.h,v 1.26 2003/01/07 22:29:21 sfeam Exp $
+ * $Id: axis.h,v 1.27 2003/01/25 10:35:31 mikulik Exp $
  *
  */
 
@@ -173,8 +173,8 @@ typedef enum e_autoscale {
     AUTOSCALE_MIN = 1<<0,
     AUTOSCALE_MAX = 1<<1,
     AUTOSCALE_BOTH = (1<<0 | 1 << 1),
-    AUTOSCALE_FIXMIN = AUTOSCALE_MIN | 1<<2,
-    AUTOSCALE_FIXMAX = AUTOSCALE_MAX | 1<<3
+    AUTOSCALE_FIXMIN = 1<<2,
+    AUTOSCALE_FIXMAX = 1<<3
 } t_autoscale;
 
 
