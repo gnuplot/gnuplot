2fe15ff25b26c293a3526abaee7bab9e6d4d4451
Petr Mikulik <mikulik@physics.muni.cz>
2003-04-18 12:41:36 +0200

    Remove "unset_loadpath()" from reset command.
    After discussion on gnuplot-beta list, the "reset" command should
    reset only graphics settings, while it should let unchanged
    non-graphics settings (like set term, out, loadpath and fontpath).

--- src/unset.c
+++ src/unset.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: unset.c,v 1.52 2003/03/13 06:17:16 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: unset.c,v 1.53 2003/04/13 20:22:46 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - unset.c */
@@ -1532,7 +1532,11 @@ reset_command()
     df_separator = '\0';
 
     unset_locale();
+#if 0
+    /* 2003-18-04: Don't reset non-graphics settings, like set term, out,
+     * loadpath and fontpath. */
     unset_loadpath();
+#endif
     unset_fit();
 
     /* HBB 20000506: set 'interactive' back to its real value: */
