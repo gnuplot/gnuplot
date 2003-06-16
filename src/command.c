0d5f56f8a75f064978dbd78d9f92ca20bf4c87cb
Petr Mikulik <mikulik@physics.muni.cz>
2003-06-16 09:30:55 +0200

    do_string_replot(): Dynamically allocate temporary input line buffer...
    ...instead of using a fix-size one. Fixes crash when replotting a long
    plot command by mouse/hotkey (in Octave, for example).

--- src/command.c
+++ src/command.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: command.c,v 1.65 2002/10/20 21:19:50 mikulik Exp $"); }
+static char *RCSid() { return RCSid("$Id: command.c,v 1.66 2002/10/25 10:06:49 mikulik Exp $"); }
 #endif
 
 /* GNUPLOT - command.c */
@@ -407,17 +407,19 @@ do_string_replot(s)
 char *s;
 {
     char *orig_input_line;
-    static char buf[256];
+    char *buf;
 
     orig_input_line = input_line;
+    buf = gp_alloc(strlen(s)+9999, "do_string_replot");
+    strcpy(buf, s);
     input_line = buf;
-    strcpy(buf,s);
     if (!replot_disabled)
-	strcat(buf,"; replot");
+	strcat(buf, "; replot");
     if (display_ipc_commands())
 	fprintf(stderr, "%s\n", buf);
     do_line();
     input_line = orig_input_line;
+    free(buf);
 }
 
 void
