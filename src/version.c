93db7418f253603f2b793912e14316831d0aa5c2
Hans-Bernhard Broeker <broeker@physik.rwth-aachen.de>
2004-03-13 00:38:02 +0100

    Up the Patchlevel to 3.8k2 for another pre-4.0 tarball.

--- src/version.c
+++ src/version.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: version.c,v 1.49 2003/08/22 12:37:39 lhecking Exp $"); }
+static char *RCSid() { return RCSid("$Id: version.c,v 1.50 2004/02/21 19:08:36 broeker Exp $"); }
 #endif
 
 /* GNUPLOT - version.c */
@@ -40,8 +40,8 @@ static char *RCSid() { return RCSid("$Id: version.c,v 1.49 2003/08/22 12:37:39 l
 
 
 const char gnuplot_version[] = "3.8k";
-const char gnuplot_patchlevel[] = "1";
-const char gnuplot_date[] = "Sat Feb 21 19:56:07 CET 2004";
+const char gnuplot_patchlevel[] = "2";
+const char gnuplot_date[] = "Thu Mar 11 23:17:49 CET 2004";
 const char gnuplot_copyright[] = "Copyright(C) 1986 - 1993, 1999 - 2003";
 
 const char faq_location[] = FAQ_LOCATION;
