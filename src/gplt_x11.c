a47f0d93a39082604b1d763a176a9b31baaa3e9a
Ethan Merritt <merritt@u.washington.edu>
2004-05-27 09:35:11 -0700

    Check for index overflow from color ramp algorithm

--- src/gplt_x11.c
+++ src/gplt_x11.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: gplt_x11.c,v 1.96 2004/05/25 04:15:09 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: gplt_x11.c,v 1.95 2004/05/21 20:10:21 sfeam Exp $"); }
 #endif
 
 #define X11_POLYLINE 1
@@ -2816,6 +2816,8 @@ PaletteSetColor(plot_struct * plot, double gray)
 
 	gray = floor(gray * plot->cmap->allocated) / (plot->cmap->allocated - 1);
 	index = gray * (plot->cmap->allocated - 1);
+	if (index >= plot->cmap->allocated)
+		index = plot->cmap->allocated -1;
 	
 	XSetForeground(dpy, gc_pm3d, plot->cmap->pixels[index]);
     }
