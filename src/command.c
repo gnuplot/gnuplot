ed7ca9a793b0b29c7c9a7b69835736b74ebeb59f
Ethan Merritt <merritt@u.washington.edu>
2004-08-09 11:07:18 -0700

    remove unused prototype

--- src/command.c
+++ src/command.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: command.c,v 1.100 2004/08/02 21:57:43 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: command.c,v 1.101 2004/08/09 00:51:29 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - command.c */
@@ -133,7 +133,6 @@ int vms_ktid;			/* key table id, for translating keystrokes */
 
 
 /* static prototypes */
-static FILE* capture_filename_and_pfopen __PROTO((char **save_file, char *mode));
 static void command __PROTO((void));
 static int changedir __PROTO((char *path));
 static char* fgets_ipc __PROTO((char* dest, int len));
