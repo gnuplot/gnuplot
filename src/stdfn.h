bc4292a26dc8dc847268a2e0007fe6bc49403038
Hans-Bernhard Broeker <broeker@users.sourceforge.net>
2005-01-26 17:16:06 +0100

    sys/param.h before checking if PATH_MAX is def
    Include sys/param.h before checking if PATH_MAX is defined;
    
    avoid redefinition.

--- src/stdfn.h
+++ src/stdfn.h
@@ -1,5 +1,5 @@
 /*
- * $Id: stdfn.h,v 1.27 2004/07/01 17:10:08 broeker Exp $
+ * $Id: stdfn.h,v 1.28 2004/11/10 23:30:30 sfeam Exp $
  */
 
 /* GNUPLOT - stdfn.h */
@@ -462,10 +462,10 @@ int gp_strnicmp __PROTO((const char *, const char *, size_t));
 #endif /* VERYLARGE */
 
 /* _POSIX_PATH_MAX is too small for practical purposes */
+#ifdef HAVE_SYS_PARAM_H
+# include <sys/param.h>
+#endif
 #ifndef PATH_MAX
-# ifdef HAVE_SYS_PARAM_H
-#  include <sys/param.h>
-# endif
 # ifndef MAXPATHLEN
 #  define PATH_MAX 1024
 # else
