994b7bab40f76ab4dfdca5769da3e0b71483f6c0
Ethan Merritt <merritt@u.washington.edu>
2005-02-02 13:05:01 -0800

    Allow set label textcolor rgb <foo>

--- src/set.c
+++ src/set.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: set.c,v 1.165 2005/01/11 17:37:14 mikulik Exp $"); }
+static char *RCSid() { return RCSid("$Id: set.c,v 1.166 2005/02/01 11:28:51 broeker Exp $"); }
 #endif
 
 /* GNUPLOT - set.c */
@@ -3777,9 +3777,9 @@ set_xyzlabel(label_struct *label)
 	    }
 #endif
 
-	/* EAM - allow to set textcolor lt <n>, but not cb frac or z */
+	/* EAM - allow set textcolor lt {<n> | rgb <foo>}, but not cb frac or z */
 	if (equals(c_token,"tc") || almost_equals(c_token,"text$color")) {
-	    parse_colorspec( &(label->textcolor), TC_LT );
+	    parse_colorspec( &(label->textcolor), TC_RGB );
 	    continue;
 	}
 
