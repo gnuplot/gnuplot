96d50a35d718904e5c86de047f151a98bff08292
Ethan A Merritt <merritt@u.washington.edu>
2006-06-28 19:22:25 -0700

    Add missing newline after 'set pm3d depthorder'

--- src/save.c
+++ src/save.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: save.c,v 1.129 2006/06/15 15:42:34 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: save.c,v 1.130 2006/06/29 19:36:43 mikulik Exp $"); }
 #endif
 
 /* GNUPLOT - save.c */
@@ -759,7 +759,7 @@ set origin %g,%g\n",
     case PM3D_SCANS_AUTOMATIC: fputs("scansautomatic\n", fp); break;
     case PM3D_SCANS_FORWARD: fputs("scansforward\n", fp); break;
     case PM3D_SCANS_BACKWARD: fputs("scansbackward\n", fp); break;
-    case PM3D_DEPTH: fputs("depthorder", fp); break;
+    case PM3D_DEPTH: fputs("depthorder\n", fp); break;
     }
     fprintf(fp, "set pm3d interpolate %d,%d", pm3d.interp_i, pm3d.interp_j);
     fputs(" flush ", fp);
