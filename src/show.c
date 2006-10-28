f8e738516789b8d648c901e92eda246401da832b
Ethan Merritt <merritt@u.washington.edu>
2006-10-27 21:22:18 -0700

    Handle textcolor of tic labels correctly for cases other than TC_LT

--- src/show.c
+++ src/show.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: show.c,v 1.185 2006/09/19 16:03:17 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: show.c,v 1.186 2006/10/21 22:58:23 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - show.c */
@@ -3105,8 +3105,9 @@ show_ticdef(AXIS_INDEX axis)
     }
 
     if (axis_array[axis].ticdef.textcolor.type != TC_DEFAULT) {
-        fprintf(stderr,"\t  textcolor lt %d\n",
-            axis_array[axis].ticdef.textcolor.lt+1);
+        fputs("\t ", stderr);
+	save_textcolor(stderr, &axis_array[axis].ticdef.textcolor);
+        fputs("\n", stderr);
     }
 
     if (axis_array[axis].ticdef.font && *axis_array[axis].ticdef.font) {
