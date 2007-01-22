a5b9ee1a3165b53fba520d224c387347cdccd355
Ethan Merritt <merritt@u.washington.edu>
2007-01-21 21:33:17 -0800

    Update versioniong to 4.2-rc3

--- src/version.c
+++ src/version.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: version.c,v 1.69.2.3 2006/12/15 08:44:10 mikulik Exp $"); }
+static char *RCSid() { return RCSid("$Id: version.c,v 1.69.2.4 2007/01/14 22:47:34 mikulik Exp $"); }
 #endif
 
 /* GNUPLOT - version.c */
@@ -40,7 +40,7 @@ static char *RCSid() { return RCSid("$Id: version.c,v 1.69.2.3 2006/12/15 08:44:
 
 
 const char gnuplot_version[] = "4.2";
-const char gnuplot_patchlevel[] = "rc2";
+const char gnuplot_patchlevel[] = "rc3";
 const char gnuplot_date[] = "January 2007";
 const char gnuplot_copyright[] = "Copyright (C) 1986 - 1993, 1998, 2004, 2007";
 
