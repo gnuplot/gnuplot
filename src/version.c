53b32564325b92b08633fd559d2a13fd24624410
Petr Mikulik <mikulik@physics.muni.cz>
2007-02-08 22:16:53 +0100

    Change date January => February.

--- src/version.c
+++ src/version.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: version.c,v 1.71 2006/11/03 15:45:25 mikulik Exp $"); }
+static char *RCSid() { return RCSid("$Id: version.c,v 1.72 2007/01/14 22:57:10 mikulik Exp $"); }
 #endif
 
 /* GNUPLOT - version.c */
@@ -41,7 +41,7 @@ static char *RCSid() { return RCSid("$Id: version.c,v 1.71 2006/11/03 15:45:25 m
 
 const char gnuplot_version[] = "4.3";
 const char gnuplot_patchlevel[] = "0";
-const char gnuplot_date[] = "January 2007";
+const char gnuplot_date[] = "February 2007";
 const char gnuplot_copyright[] = "Copyright (C) 1986 - 1993, 1998, 2004, 2007";
 
 const char faq_location[] = FAQ_LOCATION;
