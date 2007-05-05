34bc0d61c6b6b902598049b91282b6c8cb917fdf
Timothee Lecomte <timothee.lecomte@ens.fr>
2007-05-05 20:59:00 +0200

    Fix initialization of thread signal mask.

--- src/wxterminal/wxt_gui.cpp
+++ src/wxterminal/wxt_gui.cpp
@@ -1,5 +1,5 @@
 /*
- * $Id: wxt_gui.cpp,v 1.36 2007/03/31 23:33:15 sfeam Exp $
+ * $Id: wxt_gui.cpp,v 1.41 2007/04/30 12:56:08 tlecomte Exp $
  */
 
 /* GNUPLOT - wxt_gui.cpp */
@@ -178,8 +178,10 @@ void *wxtThread::Entry()
 	FPRINTF((stderr,"gui_thread_entry\n"));
 
 	/* don't answer to SIGINT in this thread - avoids LONGJMP problems */
-	sigset_t set[1] = {SIGINT};
-	pthread_sigmask(SIG_BLOCK,set,NULL);
+	sigset_t set;
+	sigemptyset(&set);
+	sigaddset(&set, SIGINT);
+	pthread_sigmask(SIG_BLOCK, &set, NULL);
 
 	/* gui loop */
 	wxTheApp->OnRun();
