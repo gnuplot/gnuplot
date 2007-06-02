9cfcbe3950f1436977a65c545599b2b3c25dccfb
Petr Mikulik <mikulik@physics.muni.cz>
2007-06-03 00:32:23 +0200

    Date set to June.

--- src/version.c
+++ src/version.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: version.c,v 1.73 2007/02/08 21:16:53 mikulik Exp $"); }
+static char *RCSid() { return RCSid("$Id: version.c,v 1.74 2007/05/30 20:35:23 mikulik Exp $"); }
 #endif
 
 /* GNUPLOT - version.c */
@@ -41,7 +41,7 @@ static char *RCSid() { return RCSid("$Id: version.c,v 1.73 2007/02/08 21:16:53 m
 
 const char gnuplot_version[] = "4.3";
 const char gnuplot_patchlevel[] = "0";
-const char gnuplot_date[] = "May 2007";
+const char gnuplot_date[] = "June 2007";
 const char gnuplot_copyright[] = "Copyright (C) 1986 - 1993, 1998, 2004, 2007";
 
 const char faq_location[] = FAQ_LOCATION;
