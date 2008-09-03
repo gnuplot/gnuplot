6eae8cb8c8738600c5180aef137bee99ce533d63
Ethan Merritt <merritt@u.washington.edu>
2008-09-03 09:28:06 -0700

    NULL labels are OK if requested via 'set xtics (x1, x2, ...)'

--- src/axis.c
+++ src/axis.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: axis.c,v 1.71 2008/08/01 16:15:28 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: axis.c,v 1.72 2008/08/19 18:48:20 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - axis.c */
@@ -1624,7 +1624,7 @@ add_tic_user(AXIS_INDEX axis, char *label, double position, int level)
     struct ticmark *tic, *newtic;
     struct ticmark listhead;
 
-    if (!label)
+    if (!label && level < 0)
 	return;
 
     /* Mark this axis as user-generated ticmarks only, unless the */
