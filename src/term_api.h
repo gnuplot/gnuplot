eac87d657ce654ac16e9a8d8d7d5f369ba6bd443
Ethan A Merritt <merritt@u.washington.edu>
2009-09-13 10:44:20 -0700

    Clean up code for "set termoption <foo>"

--- src/term_api.h
+++ src/term_api.h
@@ -1,5 +1,5 @@
 /*
- * $Id: term_api.h,v 1.81 2009/06/06 18:28:43 sfeam Exp $
+ * $Id: term_api.h,v 1.82 2009/06/08 04:37:29 sfeam Exp $
  */
 
 /* GNUPLOT - term_api.h */
@@ -171,6 +171,7 @@ typedef struct t_image {
 #define TERM_CAN_DASH       256  /* terminal supports dashed lines  */
 #define TERM_ALPHA_CHANNEL  512  /* alpha channel transparency      */
 #define TERM_MONOCHROME    1024  /* term is running in mono mode    */
+#define TERM_LINEWIDTH     2048  /* support for set term linewidth  */
 
 /* The terminal interface structure --- heart of the terminal layer.
  *
