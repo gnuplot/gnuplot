757451a7ee981bf33a4cfe6a8a1bcbd0a26d76ec
Petr Mikulik <mikulik@physics.muni.cz>
2009-10-30 23:15:39 +0100

    Workaround for vsnprintf(NULL,0,...) returning zero (MingW 3.4).

--- src/win/winmain.c
+++ src/win/winmain.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: winmain.c,v 1.28 2009/08/29 21:30:53 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: winmain.c,v 1.29 2009/09/17 20:02:39 broeker Exp $"); }
 #endif
 
 /* GNUPLOT - win/winmain.c */
@@ -94,6 +94,8 @@ static char *RCSid() { return RCSid("$Id: winmain.c,v 1.28 2009/08/29 21:30:53 s
 
 /* limits */
 #define MAXSTR 255
+#define MAXPRINTF 1024
+  /* used if vsnprintf(NULL,0,...) returns zero (MingW 3.4) */
 
 /* globals */
 TW textwin;
@@ -561,6 +563,7 @@ MyFPrintF(FILE *file, const char *fmt, ...)
 	count = _vscprintf(fmt, args) + 1;
 #else
 	count = vsnprintf(NULL,0,fmt,args) + 1;
+	if (count == 0) count = MAXPRINTF;
 #endif
 	va_end(args);
 	va_start(args, fmt);
@@ -588,6 +591,7 @@ MyVFPrintF(FILE *file, const char *fmt, va_list args)
 	count = _vscprintf(fmt, args_copied) + 1;
 #else
 	count = vsnprintf(NULL, 0U, fmt, args_copied) + 1;
+	if (count == 0) count = MAXPRINTF;
 #endif
 	va_end(args_copied);
 	buf = (char *)malloc(count * sizeof(char));
@@ -611,6 +615,7 @@ MyPrintF(const char *fmt, ...)
     count = _vscprintf(fmt, args) + 1;
 #else
     count = vsnprintf(NULL, 0, fmt, args) + 1;
+    if (count == 0) count = MAXPRINTF;
 #endif
     va_end(args);
     va_start(args, fmt);
