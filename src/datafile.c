6ff30775ee7d896f03064a6a679081b4a97c32fd
Ethan A Merritt <merritt@u.washington.edu>
2009-12-02 20:45:33 -0800

    Enforce restrictions on which plot types can accept matrix data

--- src/datafile.c
+++ src/datafile.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: datafile.c,v 1.172.2.4 2009/09/03 04:45:04 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: datafile.c,v 1.172.2.5 2009/10/10 18:45:30 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - datafile.c */
@@ -2091,7 +2091,7 @@ f_stringcolumn(union argument *arg)
     (void) pop(&a);
     column = (int) real(&a);
 
-    if (!evaluate_inside_using)
+    if (!evaluate_inside_using || df_matrix)
 	int_error(c_token-1, "stringcolumn() called from invalid context");
 
     if (column < 1 || column > df_no_cols) {
--- src/plot2d.c
+++ src/plot2d.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: plot2d.c,v 1.193.2.2 2009/08/19 15:52:31 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: plot2d.c,v 1.193.2.3 2009/10/23 15:34:19 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - plot2d.c */
@@ -1861,6 +1861,14 @@ eval_plots()
 		if ((this_plot->plot_style & PLOT_STYLE_HAS_POINT)
 		&&  (this_plot->lp_properties.p_size == PTSZ_VARIABLE))
 		    this_plot->lp_properties.p_size = 1;
+	    }
+
+	    if (df_matrix) {
+		if (!(this_plot->plot_style == IMAGE || this_plot->plot_style == RGBIMAGE
+	              || this_plot->plot_style == RGBA_IMAGE || this_plot->plot_style == LINES
+		      || this_plot->plot_style == POINTSTYLE))
+		int_error(NO_CARET, "this 2D plot style cannot handle matrix data");
+
 	    }
 
 	    /* Similar argument for check that all fill styles were set */
