fc06488efcd660c6aa232cec0660fcae4bdf49cb
Petr Mikulik <mikulik@physics.muni.cz>
2010-02-03 11:00:33 +0100

    January => February 2010.

--- src/version.c
+++ src/version.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: version.c,v 1.89.2.3 2010/01/25 07:59:33 mikulik Exp $"); }
+static char *RCSid() { return RCSid("$Id: version.c,v 1.89.2.4 2010/01/27 09:13:32 mikulik Exp $"); }
 #endif
 
 /* GNUPLOT - version.c */
@@ -41,7 +41,7 @@ static char *RCSid() { return RCSid("$Id: version.c,v 1.89.2.3 2010/01/25 07:59:
 
 const char gnuplot_version[] = "4.4";
 const char gnuplot_patchlevel[] = "0-rc1";
-const char gnuplot_date[] = "January 2010";
+const char gnuplot_date[] = "February 2010";
 const char gnuplot_copyright[] = "Copyright (C) 1986-1993, 1998, 2004, 2007-2010";
 
 char *compile_options = (void *)0;	/* Will be loaded at runtime */
