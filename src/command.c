e3971d8d20e33d1e61a76364c84bdc4aebc5361f
Tatsuro Matsuoka <tmacchant3@yahoo.co.jp>
2010-02-03 10:55:44 +0100

    Loop of waitforinput() for "pause -1" in console mode of Windows.

--- src/command.c
+++ src/command.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: command.c,v 1.189 2010/01/31 20:27:45 mikulik Exp $"); }
+static char *RCSid() { return RCSid("$Id: command.c,v 1.190 2010/02/01 06:56:13 mikulik Exp $"); }
 #endif
 
 /* GNUPLOT - command.c */
@@ -1150,7 +1150,7 @@ pause_command()
 	    } else {
 #  if defined(WGP_CONSOLE)
 		fprintf(stderr,"%s\n", buf);
-		term->waitforinput();
+		while (term->waitforinput() != (int)'\r') {}; /* waiting for Enter*/
 #  else /* !WGP_CONSOLE */
 		if (!Pause(buf)) 
 		bail_to_command_line();
@@ -1170,7 +1170,7 @@ pause_command()
 		if (!tmp) {
 #  if defined(WGP_CONSOLE)
 		    fprintf(stderr,"%s\n", buf);
-		    term->waitforinput(); 
+		    while (term->waitforinput() != (int)'\r') {}; /* waiting for Enter*/
 #  else
 		    if (!Pause(buf)) 
 		       bail_to_command_line();
