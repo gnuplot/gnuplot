2f1441646846570dfd29f20f35611d3ab27fc925
Ethan A Merritt <merritt@u.washington.edu>
2010-08-24 22:09:02 -0700

    Allow "fc <colorspec>" as an in-line option to the plot command

--- src/plot2d.c
+++ src/plot2d.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: plot2d.c,v 1.221 2010/08/07 20:27:57 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: plot2d.c,v 1.222 2010/08/07 20:51:26 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - plot2d.c */
@@ -2007,8 +2007,8 @@ eval_plots()
 
 		/* Some plots have a fill style as well */
 		if (this_plot->plot_style & PLOT_STYLE_HAS_FILL){
+		    int stored_token = c_token;
 		    if (equals(c_token,"fs") || almost_equals(c_token,"fill$style")) {
-			int stored_token = c_token;
 			parse_fillstyle(&this_plot->fill_properties,
 				default_fillstyle.fillstyle,
 				default_fillstyle.filldensity,
@@ -2018,9 +2018,14 @@ eval_plots()
 			&& this_plot->fill_properties.fillstyle == FS_EMPTY)
 			    this_plot->fill_properties.fillstyle = FS_SOLID;
 			set_fillstyle = TRUE;
-			if (stored_token != c_token)
-			    continue;
 		    }
+		    if (equals(c_token,"fc") || almost_equals(c_token,"fillc$olor")) {
+			parse_colorspec(&this_plot->lp_properties.pm3d_color,TC_Z);
+			this_plot->lp_properties.use_palette = TRUE;
+			set_lpstyle = TRUE;
+		    }
+		    if (stored_token != c_token)
+			continue;
 		}
 
 		break; /* unknown option */
