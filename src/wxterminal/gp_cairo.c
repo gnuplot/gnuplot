8dc37f715e44974a03a42c7576fe37c52b031dbd
Ethan A Merritt <merritt@u.washington.edu>
2010-08-26 11:18:27 -0700

    Limit overshoot of lines meeting at acute angles

--- src/wxterminal/gp_cairo.c
+++ src/wxterminal/gp_cairo.c
@@ -1,5 +1,5 @@
 /*
- * $Id: gp_cairo.c,v 1.48.2.2 2009/11/07 03:39:40 sfeam Exp $
+ * $Id: gp_cairo.c,v 1.48.2.3 2010/04/05 05:38:00 sfeam Exp $
  */
 
 /* GNUPLOT - gp_cairo.c */
@@ -221,6 +221,7 @@ void gp_cairo_initialize_context(plot_struct *plot)
 	} else {
 	    cairo_set_line_cap  (plot->cr, CAIRO_LINE_CAP_SQUARE);
 	    cairo_set_line_join (plot->cr, CAIRO_LINE_JOIN_MITER);
+	    cairo_set_miter_limit(plot->cr, 3.8);
 	}
 
 }
