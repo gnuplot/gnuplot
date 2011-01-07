fe8ab7e20fba0765da2c253d44d4384448423d99
Peter Juhasz <juhaszp@users.sourceforge.net>
2011-01-07 18:24:25 +0000

    "reset" didn't restore default of "set raxis"

--- src/unset.c
+++ src/unset.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: unset.c,v 1.140 2010/11/18 23:59:59 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: unset.c,v 1.141 2010/12/05 00:01:02 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - unset.c */
@@ -1708,6 +1708,7 @@ reset_command()
 
 	reset_logscale(axis);
     }
+    raxis = TRUE;
 
     unset_boxplot();
     unset_boxwidth();
