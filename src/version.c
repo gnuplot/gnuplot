aca450224a3a9e8e6b27159f53e88c4cdeb855ee
Ethan A Merritt <merritt@u.washington.edu>
2011-03-24 12:44:19 -0700

    timestamp as March 2011

--- src/version.c
+++ src/version.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: version.c,v 1.97 2010/08/14 17:21:54 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: version.c,v 1.98 2010/09/18 05:25:17 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - version.c */
@@ -41,7 +41,7 @@ static char *RCSid() { return RCSid("$Id: version.c,v 1.97 2010/08/14 17:21:54 s
 
 const char gnuplot_version[] = "4.5";
 const char gnuplot_patchlevel[] = "0";
-const char gnuplot_date[] = "September 2010";
+const char gnuplot_date[] = "March 2011";
 const char gnuplot_copyright[] = "Copyright (C) 1986-1993, 1998, 2004, 2007-2010";
 
 const char faq_location[] = FAQ_LOCATION;
