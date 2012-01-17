9f0c44d05d2d8b12cd035f5a81e474b079e99124
Ethan A Merritt <merritt@u.washington.edu>
2012-01-17 11:16:53 -0800

    Store the canvas size in GPVAL_TERM_XSIZE, GPVAL_TERM_YSIZE

--- src/eval.c
+++ src/eval.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: eval.c,v 1.97 2011/11/24 05:08:35 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: eval.c,v 1.98 2011/11/26 13:37:30 markisch Exp $"); }
 #endif
 
 /* GNUPLOT - eval.c */
@@ -809,6 +809,8 @@ update_plot_bounds(void)
     fill_gpval_integer("GPVAL_TERM_XMAX", axis_array[FIRST_X_AXIS].term_upper / term->tscale);
     fill_gpval_integer("GPVAL_TERM_YMIN", axis_array[FIRST_Y_AXIS].term_lower / term->tscale);
     fill_gpval_integer("GPVAL_TERM_YMAX", axis_array[FIRST_Y_AXIS].term_upper / term->tscale);
+    fill_gpval_integer("GPVAL_TERM_XSIZE", canvas.xright+1);
+    fill_gpval_integer("GPVAL_TERM_YSIZE", canvas.ytop+1);
 }
 
 /*
