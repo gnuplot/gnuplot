f85792bc4b8bdad3041be1d5b9e564fc11b57ad3
Ethan A Merritt <merritt@u.washington.edu>
2012-10-29 17:45:56 -0700

    save axis scaling for map view 3D plots: cleanup

--- src/graph3d.c
+++ src/graph3d.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: graph3d.c,v 1.272 2012/10/28 22:21:35 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: graph3d.c,v 1.273 2012/10/29 00:13:20 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - graph3d.c */
@@ -708,16 +708,8 @@ do_3dplot(
 	map3d_xy(X->max, Y->max, 0.0, &xr, &yt);
 	AXIS_SETSCALE(FIRST_X_AXIS, xl, xr);
 	AXIS_SETSCALE(FIRST_Y_AXIS, yb, yt);
-	X->term_lower = xl;
-	Y->term_lower = yb;
-	FPRINTF((stderr,"X: left right min max scale\n    %d %d %g %g %g\n",
-	    xl, xr,
-	    axis_array[FIRST_X_AXIS].min, axis_array[FIRST_X_AXIS].max,
-	    axis_array[FIRST_X_AXIS].term_scale));
-	FPRINTF((stderr,"Y: bottom top min max scale\n    %d %d %g %g %g\n",
-	    yb, yt,
-	    axis_array[FIRST_Y_AXIS].min, axis_array[FIRST_Y_AXIS].max,
-	    axis_array[FIRST_Y_AXIS].term_scale));
+	axis_set_graphical_range(FIRST_X_AXIS, xl, xr);
+	axis_set_graphical_range(FIRST_Y_AXIS, yb, yt);
     }
 
     /* Initialize palette */
