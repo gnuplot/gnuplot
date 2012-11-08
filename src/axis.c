6ef4a80e6eec486c9c354b79d6d2cc4cfbcb981b
Ethan A Merritt <merritt@u.washington.edu>
2012-11-08 09:13:08 -0800

    Do not truncate axis tic labels provided as a string constant

--- src/axis.c
+++ src/axis.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: axis.c,v 1.97.2.1 2012/04/03 16:57:40 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: axis.c,v 1.97.2.2 2012/09/26 23:05:56 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - axis.c */
@@ -881,6 +881,7 @@ gen_tics(AXIS_INDEX axis, tic_callback callback)
 
 	for (mark = def->def.user; mark; mark = mark->next) {
 	    char label[MAX_ID_LEN];
+	    char *ticlabel;
 	    double internal;
 
 	    /* This condition is only possible if we are in polar mode */
@@ -893,16 +894,23 @@ gen_tics(AXIS_INDEX axis, tic_callback callback)
 	    if (!inrange(internal, internal_min, internal_max))
 		continue;
 
-	    if (mark->level < 0) /* label read from data file */
-		strncpy(label, mark->label, sizeof(label));
-	    else if (axis_array[axis].datatype == DT_TIMEDATE)
+	    if (mark->level < 0) {
+		/* label read from data file */
+		ticlabel = mark->label;
+	    } else if (mark->label && !strchr(mark->label, '%')) {
+		/* string constant that contains no format keys */
+		ticlabel = mark->label;
+	    } else if (axis_array[axis].datatype == DT_TIMEDATE) {
 		gstrftime(label, MAX_ID_LEN-1, mark->label ? mark->label : ticfmt[axis], mark->position);
-	    else
+		ticlabel = label;
+	    } else {
 		gprintf(label, sizeof(label), mark->label ? mark->label : ticfmt[axis], log10_base, mark->position);
+		ticlabel = label;
+	    }
 
 	    /* use NULL instead of label for minitic */
 	    (*callback) (axis, internal,
-	    		(mark->level>0)?NULL:label,
+	    		(mark->level>0)?NULL:ticlabel,
 	    		(mark->level>0)?mgrd:lgrd, NULL);
 
 	    /* Polar axis tics are mirrored across the origin */
@@ -910,7 +918,7 @@ gen_tics(AXIS_INDEX axis, tic_callback callback)
 		int save_gridline = lgrd.l_type;
 		lgrd.l_type = LT_NODRAW;
 		(*callback) (axis, -internal,
-			(mark->level>0)?NULL:label,
+			(mark->level>0)?NULL:ticlabel,
 	    		(mark->level>0)?mgrd:lgrd, NULL);
 		lgrd.l_type = save_gridline;
 	    }
