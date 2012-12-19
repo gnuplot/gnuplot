d2c2d0e983b34aeecd26d081422f4c05b0e94415
Olaf Booij <obooij@users.sourceforge.net>
2012-12-18 22:15:06 -0800

    zoom about center of screen in 3D mode "set view equal xya"

--- src/graph3d.c
+++ src/graph3d.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: graph3d.c,v 1.275 2012/11/29 00:12:56 broeker Exp $"); }
+static char *RCSid() { return RCSid("$Id: graph3d.c,v 1.276 2012/12/17 04:00:40 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - graph3d.c */
@@ -696,6 +696,10 @@ do_3dplot(
 	if (aspect_ratio_3D >= 3)
 	    zscale3d = xscale3d;
     }
+
+    /* Without this the rotation center would be located at */
+    /* the bottom of the plot. This places it in the middle.*/
+    zcenter3d =  -(ceiling_z - floor_z) / 2.0 * zscale3d + 1;
 
     /* Needed for mousing by outboard terminal drivers */
     if (splot_map) {
