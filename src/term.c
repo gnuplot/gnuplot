ba65e145b5ef3ee65a69c871688d5222f8780e72
Ethan A Merritt <merritt@u.washington.edu>
2013-02-21 17:57:59 -0800

    remove vestigial support for SCO cgi graphics layer

--- src/term.c
+++ src/term.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: term.c,v 1.248 2013/01/05 23:15:57 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: term.c,v 1.249 2013/01/09 05:23:22 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - term.c */
@@ -1829,11 +1829,6 @@ init_terminal()
 	    term_name = "unixpc";
 	}
 #endif /* unixpc */
-
-#ifdef CGI
-	if (getenv("CGIDISP") || getenv("CGIPRNT"))
-	    term_name = "cgi";
-#endif /*CGI */
 
 #ifdef DJGPP
 	term_name = "svga";
