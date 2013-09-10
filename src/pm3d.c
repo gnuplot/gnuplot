3945266d46b2b9c96e3d064b9579b7ca4b88f2ed
Ethan A Merritt <merritt@u.washington.edu>
2013-09-10 13:39:37 -0700

    tc variable should not trigger display of color palette

--- src/pm3d.c
+++ src/pm3d.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: pm3d.c,v 1.85.2.1 2012/03/31 05:54:38 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: pm3d.c,v 1.85.2.2 2012/07/05 00:30:07 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - pm3d.c */
@@ -1115,7 +1115,9 @@ set_plot_with_palette(int plot_num, int plot_mode)
 	    if (this_2dplot->plot_style == IMAGE)
 		return;
 	    if (this_2dplot->lp_properties.use_palette
-	    &&  this_2dplot->lp_properties.pm3d_color.type > TC_RGB)
+	    && (this_2dplot->lp_properties.pm3d_color.type == TC_CB
+	    ||  this_2dplot->lp_properties.pm3d_color.type == TC_FRAC
+	    ||  this_2dplot->lp_properties.pm3d_color.type == TC_Z))
 		return;
 	    if (this_2dplot->labels
 	    && (this_2dplot->labels->textcolor.type == TC_CB
