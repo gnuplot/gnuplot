fb67a46a14519d55f12032fd6eaf393c86293927
Ethan A Merritt <merritt@u.washington.edu>
2014-01-27 15:59:59 -0800

    increase precision of xticlabel placement from (float) to (double)

--- src/datafile.c
+++ src/datafile.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: datafile.c,v 1.212.2.27 2014/01/03 21:40:42 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: datafile.c,v 1.212.2.28 2014/01/10 23:26:15 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - datafile.c */
@@ -1814,7 +1814,7 @@ df_readascii(double v[], int max)
 		/* Axis labels, plot titles, etc.                     */
 		if (use_spec[output].expected_type >= CT_XTICLABEL) {
 		    int axis, axcol;
-		    float xpos;
+		    double xpos;
 		   
 		    /* EAM FIXME - skip columnstacked histograms also */
 		    if (df_current_plot) {
