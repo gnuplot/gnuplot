28fa6739552ead98f5600a8feee2947c5fee1a75
Ethan A Merritt <merritt@u.washington.edu>
2014-04-02 12:41:39 -0700

    Accept "with image pixels" as a synonym for "with image failsafe"

--- src/misc.c
+++ src/misc.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: misc.c,v 1.138.2.15 2013/07/22 22:19:11 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: misc.c,v 1.138.2.16 2013/10/27 22:13:43 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - misc.c */
@@ -1342,7 +1342,7 @@ arrow_parse(
 void
 get_image_options(t_image *image)
 {
-    if (equals(c_token, "failsafe")) {
+    if (almost_equals(c_token, "pix$els") || equals(c_token, "failsafe")) {
 	c_token++;
 	image->fallback = TRUE;
     }
