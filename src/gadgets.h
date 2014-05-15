d67a312246d441177cc0f4c41e35dc395fc91f5c
Ethan A Merritt <merritt@u.washington.edu>
2014-05-15 16:37:41 -0700

    Bump version information to 5.0.rc1

--- src/gadgets.h
+++ src/gadgets.h
@@ -182,7 +182,7 @@ typedef struct object {
 
 /* Datastructure implementing 'set dashtype' */
 struct custom_dashtype_def {
-    struct custom_dashtype_def *next;	/* pointer to next linestyle in linked list */
+    struct custom_dashtype_def *next;	/* pointer to next dashtype in linked list */
     int tag;			/* identifies the dashtype */
     int d_type;                 /* for DASHTYPE_SOLID or CUSTOM */
     struct t_dashtype dashtype;
