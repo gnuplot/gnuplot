64ee12b81bf2ad961ef019389ab07d679af3004f
Ethan A Merritt <merritt@u.washington.edu>
2014-08-05 10:14:03 -0700

    remove extraneous event_reset

--- src/mouse.c
+++ src/mouse.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: mouse.c,v 1.133.2.6 2014/06/11 22:19:17 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: mouse.c,v 1.133.2.7 2014/08/01 21:31:01 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - mouse.c */
@@ -1624,7 +1624,6 @@ event_buttonpress(struct gp_event_t *ge)
 	      /* "pause button1" or "pause any" takes precedence over key bindings */
 		if (paused_for_mouse & PAUSE_BUTTON1) {
 		    load_mouse_variables(mouse_x, mouse_y, TRUE, b);
-		    event_reset(ge);
 		    trap_release = TRUE;	/* Don't trigger on release also */
 		    return;
 		}
