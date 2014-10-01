8c1a15183bd8028149ecb62d46023e5cbe9eb8be
Ethan A Merritt <merritt@u.washington.edu>
2014-09-30 21:30:44 -0700

    timedata for HISTOGRAMS is a special case

--- src/plot2d.c
+++ src/plot2d.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: plot2d.c,v 1.337 2014/09/05 21:51:38 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: plot2d.c,v 1.338 2014/09/07 16:53:38 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - plot2d.c */
@@ -600,10 +600,13 @@ get_data(struct curve_points *current_plot)
 
     /* EXPERIMENTAL May 2013 - Treating timedata columns as strings allows */
     /* functions column(N) and column("HEADER") to work on time data.	   */
-    if (axis_array[current_plot->x_axis].datatype == DT_TIMEDATE)
-	expect_string(1);
-    if (axis_array[current_plot->y_axis].datatype == DT_TIMEDATE)
-	expect_string(2);
+    /* Sep 2014: But the column count is wrong for HISTOGRAMS */
+    if (current_plot->plot_style != HISTOGRAMS) {
+	if (axis_array[current_plot->x_axis].datatype == DT_TIMEDATE)
+	    expect_string(1);
+	if (axis_array[current_plot->y_axis].datatype == DT_TIMEDATE)
+	    expect_string(2);
+    }
 
     if (df_no_use_specs > max_cols)
 	int_error(NO_CARET, "Too many using specs for this style");
