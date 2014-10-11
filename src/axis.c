034e812554d06eed97648673f409f111bd31f277
Hans-Bernhard Broeker <broeker@physik.rwth-aachen.de>
2014-10-11 15:56:45 +0200

    Bug #1496: extend auto-invented format to support ranges longer than one decade.

--- src/axis.c
+++ src/axis.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: axis.c,v 1.136 2014/10/04 22:22:27 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: axis.c,v 1.137 2014/10/04 23:28:38 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - axis.c */
@@ -511,7 +511,8 @@ copy_or_invent_formatstring(AXIS_INDEX axis)
 	if (!strcmp(ticfmt[axis], DEF_FORMAT)) {
 	    double axmin = AXIS_DE_LOG_VALUE(axis,axis_array[axis].min);
 	    double axmax = AXIS_DE_LOG_VALUE(axis,axis_array[axis].max);
-	    int precision = (ceil(-log10(fabs(axmax-axmin))));
+	    int precision = ceil(-log10(GPMIN(fabs(axmax-axmin),fabs(axmin))));
+
 	    if ((axmin*axmax > 0) && precision > 4)
 		sprintf(ticfmt[axis],"%%.%df", (precision>14) ? 14 : precision);
 	}
