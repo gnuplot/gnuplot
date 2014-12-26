45b4cf5c43a0cacfa174eec52da51b03b5c99447
Ethan A Merritt <merritt@u.washington.edu>
2014-12-26 15:23:03 -0800

    remove old debug code

--- src/wxterminal/wxt_gui.h
+++ src/wxterminal/wxt_gui.h
@@ -1,5 +1,5 @@
 /*
- * $Id: wxt_gui.h,v 1.48.2.2 2014/11/08 03:08:42 sfeam Exp $
+ * $Id: wxt_gui.h,v 1.48.2.3 2014/12/05 18:04:00 sfeam Exp $
  */
 
 /* GNUPLOT - wxt_gui.h */
@@ -545,11 +545,6 @@ static wxCursor wxt_cursor_cross;
 static wxCursor wxt_cursor_right;
 static wxCursor wxt_cursor_rotate;
 static wxCursor wxt_cursor_size;
-
-#ifdef DEBUG
- /* performance watch */
- static wxStopWatch sw;
-#endif
 
 /* wxt_abort_init is set to true if there is an error when
  * wxWidgets is initialized, for example if the X server is unreachable.
