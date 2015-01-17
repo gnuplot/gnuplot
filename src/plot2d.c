79044196f8bfb24109dcbe9bab3ffbae5ebf5ecc
Ethan A Merritt <merritt@u.washington.edu>
2015-01-17 10:17:38 -0800

    no autoscale for filledcurve y1=foo

--- src/plot2d.c
+++ src/plot2d.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: plot2d.c,v 1.336.2.8 2015/01/05 22:16:46 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: plot2d.c,v 1.336.2.9 2015/01/17 05:36:13 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - plot2d.c */
@@ -3197,7 +3197,7 @@ eval_plots()
 				    this_plot->points[i].yhigh,
 				    this_plot->filledcurves_options.at,
 				    this_plot->points[i].type, y_axis,
-				    this_plot->noautoscale, NOOP, NOOP);
+				    TRUE, NOOP, NOOP);
 			    }
 			    
 			    /* Fill in additional fields needed to draw a circle */
