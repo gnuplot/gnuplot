ecdbb97a9791fccd870a6ced91572f115bc30c9e
Ethan A Merritt <merritt@u.washington.edu>
2015-08-13 22:47:29 -0700

    include stdint.h to pick up type int32_t

--- src/stdfn.h
+++ src/stdfn.h
@@ -1,5 +1,5 @@
 /*
- * $Id: stdfn.h,v 1.48 2014/04/28 04:35:29 sfeam Exp $
+ * $Id: stdfn.h,v 1.49 2014/05/01 19:50:15 broeker Exp $
  */
 
 /* GNUPLOT - stdfn.h */
@@ -133,6 +133,10 @@ double strtod();
 #  endif
 # endif /* VMS */
 #endif /* HAVE_STDLIB_H */
+
+#ifdef HAVE_STDINT_H
+#include <stdint.h>
+#endif
 
 /* Deal with varargs functions */
 #if defined(HAVE_VFPRINTF) || defined(HAVE_DOPRNT)
