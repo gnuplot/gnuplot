600a98a5c1711f1ab7bf949c2720cb469ec56620
Hans-Bernhard Broeker <broeker@physik.rwth-aachen.de>
2015-10-13 23:47:34 +0200

    Add jitter to OpenWatcom makefile; fix initializer of its state variable

--- src/jitter.c
+++ src/jitter.c
@@ -1,5 +1,5 @@
 /*
- * $Id: jitter.c,v 1.0 2015/09/13 19:30:52 sfeam Exp $
+ * $Id: jitter.c,v 1.1 2015/09/14 03:28:47 sfeam Exp $
  */
 /*
  * This file contains routines used to support the "set jitter" option.
@@ -66,7 +66,7 @@
 
 #include "jitter.h"
 
-t_jitter jitter = {};
+t_jitter jitter = {{first_axes, first_axes, first_axes, 0.0, 0.0, 0.0}, 0.0, 0.0, JITTER_DEFAULT};
 
 static int compare_xypoints __PROTO((SORTFUNC_ARGS arg1, SORTFUNC_ARGS arg2));
 
