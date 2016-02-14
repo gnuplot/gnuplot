da0bd9aa3a81cadb58303d7c62908b567c32eb58
Bastian Maerkisch <bmaerkisch@web.de>
2016-02-14 10:54:27 +0100

    Use local events to avoid warnings about incompatible linkage on Windows.

--- src/wxterminal/wxt_gui.h
+++ src/wxterminal/wxt_gui.h
@@ -1,5 +1,5 @@
 /*
- * $Id: wxt_gui.h,v 1.53 2015/05/13 19:15:58 sfeam Exp $
+ * $Id: wxt_gui.h,v 1.54 2016/02/14 08:51:55 markisch Exp $
  */
 
 /* GNUPLOT - wxt_gui.h */
@@ -240,15 +240,15 @@ public:
 static wxtThread * thread;
 #endif /* WXT_MULTITHREADED */
 
-DECLARE_EVENT_TYPE(wxExitLoopEvent, -1)
-DEFINE_EVENT_TYPE(wxExitLoopEvent)
+DECLARE_LOCAL_EVENT_TYPE(wxExitLoopEvent, -1)
+DEFINE_LOCAL_EVENT_TYPE(wxExitLoopEvent)
 
-DECLARE_EVENT_TYPE(wxCreateWindowEvent, -1)
-DEFINE_EVENT_TYPE(wxCreateWindowEvent)
+DECLARE_LOCAL_EVENT_TYPE(wxCreateWindowEvent, -1)
+DEFINE_LOCAL_EVENT_TYPE(wxCreateWindowEvent)
 
 #ifdef USE_MOUSE
-DECLARE_EVENT_TYPE(wxStatusTextEvent, -1)
-DEFINE_EVENT_TYPE(wxStatusTextEvent)
+DECLARE_LOCAL_EVENT_TYPE(wxStatusTextEvent, -1)
+DEFINE_LOCAL_EVENT_TYPE(wxStatusTextEvent)
 #endif /* USE_MOUSE */
 
 /* Define a new application type, each gui should derive a class from wxApp */
