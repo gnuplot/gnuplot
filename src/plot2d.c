da4e7bcc55365c40eb33ac17249f70d81bca782d
Ethan A Merritt <merritt@u.washington.edu>
2016-03-16 21:22:32 -0700

    If pointtype PT_CHARACTER is the default for a plot...
    ...then allow text options like "tc rgb 'foo'" in the plot command.

--- src/plot2d.c
+++ src/plot2d.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: plot2d.c,v 1.336.2.19 2016/01/11 23:32:50 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: plot2d.c,v 1.336.2.20 2016/01/18 23:51:13 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - plot2d.c */
@@ -2674,6 +2674,7 @@ eval_plots()
 		if (this_plot->labels == NULL) {
 		    this_plot->labels = new_text_label(-1);
 		    this_plot->labels->pos = CENTRE;
+		    parse_label_options(this_plot->labels, 2);
 		}
 	    }
 
