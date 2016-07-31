d6e722d8114290b95f8920de8fae0648a8ffe983
Bastian Maerkisch <bmaerkisch@web.de>
2016-07-31 13:52:42 +0200

    Handle errors when creating a cairo surfaces. Bug #1621

--- src/wxterminal/wxt_gui.cpp
+++ src/wxterminal/wxt_gui.cpp
@@ -1,5 +1,5 @@
 /*
- * $Id: wxt_gui.cpp,v 1.154 2016/02/11 14:06:27 markisch Exp $
+ * $Id: wxt_gui.cpp,v 1.155 2016/02/14 08:51:56 markisch Exp $
  */
 
 /* GNUPLOT - wxt_gui.cpp */
@@ -551,6 +551,11 @@ void wxtFrame::OnExport( wxCommandEvent& WXUNUSED( event ) )
 		surface = cairo_pdf_surface_create(
 			fullpathFilename.mb_str(wxConvUTF8),
 			wxt_current_plot->device_xmax, wxt_current_plot->device_ymax);
+		if (cairo_surface_status(surface) != CAIRO_STATUS_SUCCESS) {
+			fprintf(stderr, "Cairo error: could not create surface for file %s.\n", (const char *)fullpathFilename.mb_str());
+			cairo_surface_destroy(surface);
+			break;
+		}
 		wxt_current_plot->cr = cairo_create(surface);
 		cairo_surface_destroy(surface);
 
@@ -573,6 +578,11 @@ void wxtFrame::OnExport( wxCommandEvent& WXUNUSED( event ) )
 		surface = cairo_svg_surface_create(
 			fullpathFilename.mb_str(wxConvUTF8),
 			wxt_current_plot->device_xmax, wxt_current_plot->device_ymax);
+		if (cairo_surface_status(surface) != CAIRO_STATUS_SUCCESS) {
+			fprintf(stderr, "Cairo error: could not create surface for file %s.\n", (const char *)fullpathFilename.mb_str());
+			cairo_surface_destroy(surface);
+			break;
+		}
 		wxt_current_plot->cr = cairo_create(surface);
 		cairo_surface_destroy(surface);
 
