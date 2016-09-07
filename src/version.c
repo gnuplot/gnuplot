a8abdff12682c6c5d2edc21b2e07bc5c12fad282
Ethan A Merritt <merritt@u.washington.edu>
2016-09-06 21:14:29 -0700

    bump patchlevel to 5pre

--- src/version.c
+++ src/version.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: version.c,v 1.111.2.13 2016/07/15 06:08:34 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: version.c,v 1.111.2.14 2016/07/21 19:00:31 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - version.c */
@@ -40,11 +40,11 @@ static char *RCSid() { return RCSid("$Id: version.c,v 1.111.2.13 2016/07/15 06:0
 
 
 const char gnuplot_version[] = "5.0";
-const char gnuplot_patchlevel[] = "4";
+const char gnuplot_patchlevel[] = "5pre";
 #ifdef DEVELOPMENT_VERSION
 #include "timestamp.h"
 #else
-const char gnuplot_date[] = "2016-07-21 ";
+const char gnuplot_date[] = "2016-09-07 ";
 #endif
 const char gnuplot_copyright[] = "Copyright (C) 1986-1993, 1998, 2004, 2007-2016";
 
