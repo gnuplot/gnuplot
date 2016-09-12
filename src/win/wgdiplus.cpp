efe894f0485e55453e27c2b551458f7483f25c8a
Bastian Maerkisch <bmaerkisch@web.de>
2016-09-12 17:14:15 +0200

    Fix a resource leak.

--- src/win/wgdiplus.cpp
+++ src/win/wgdiplus.cpp
@@ -1,5 +1,5 @@
 /*
- * $Id: wgdiplus.cpp,v 1.16.2.7 2016/07/31 12:26:30 markisch Exp $
+ * $Id: wgdiplus.cpp,v 1.16.2.8 2016/09/12 15:05:21 markisch Exp $
  */
 
 /*
@@ -1556,5 +1556,7 @@ drawgraph_gdiplus(LPGW lpgw, HDC hdc, LPRECT rect)
 		delete pattern_brush;
 	if (cb)
 		delete cb;
+	if (font)
+		delete font;
 	LocalFreePtr(ppt);
 }
