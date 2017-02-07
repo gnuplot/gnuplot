90b1dd790e2cff84d418e15ef4f920af7c539ad4
Ethan A Merritt <merritt@u.washington.edu>
2017-02-07 13:40:43 -0800

    hidden3d structures must be initialized for dots as they are for points

--- src/hidden3d.c
+++ src/hidden3d.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: hidden3d.c,v 1.113 2016/04/16 04:01:06 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: hidden3d.c,v 1.114 2017/02/02 06:10:45 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - hidden3d.c */
@@ -1116,14 +1116,15 @@ build_networks(struct surface_points *plots, int pcount)
 	    nv += 2 * nverts;
 	    ne += nverts;
 	    break;
-	case DOTS:
-	    this_plot->lp_properties.flags |= LP_SHOW_POINTS;
-	    this_plot->lp_properties.p_type = -1;
 	case IMAGE:
 	case RGBIMAGE:
 	case RGBA_IMAGE:
 	    /* Ignore these */
 	    break;
+	case DOTS:
+	    this_plot->lp_properties.flags |= LP_SHOW_POINTS;
+	    this_plot->lp_properties.p_type = -1;
+	    /* fall through */
 	case POINTSTYLE:
 	default:
 	    /* treat all remaining ones like 'points' */
