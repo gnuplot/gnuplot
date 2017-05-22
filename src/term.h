bf358f9c438c478cf848ca7c6af0a5368c49ab2a
Ethan A Merritt <merritt@u.washington.edu>
2017-05-22 13:39:19 -0700

    restore dxf terminal in default build

--- src/term.h
+++ src/term.h
@@ -1,5 +1,5 @@
 /*
- * $Id: term.h,v 1.71 2016/11/17 22:57:20 sfeam Exp $
+ * $Id: term.h,v 1.72 2017/05/18 21:18:57 sfeam Exp $
  */
 
 /* GNUPLOT - term.h */
@@ -234,9 +234,9 @@
 # include "caca.trm"
 #endif
 
-/* Legacy terminal for export to AutoCad (Release 10.x) */
+/* Terminal for export to AutoCad (Release 10.x) */
 /* DWGR10 format (1988) */
-/* #include "dxf.trm" */
+#include "dxf.trm"
 
 /* Enhanced Metafile Format driver */
 #include "emf.trm"
