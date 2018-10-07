3a62a7533537cab5e409fcd64f7ddc33d8915c32
Ethan A Merritt <merritt@u.washington.edu>
2018-10-06 21:49:47 -0700

    Release 5.2.5

--- src/version.c
+++ src/version.c
@@ -36,11 +36,11 @@
 
 
 const char gnuplot_version[] = "5.2";
-const char gnuplot_patchlevel[] = "5a";
+const char gnuplot_patchlevel[] = "5";
 #ifdef DEVELOPMENT_VERSION
 #include "timestamp.h"
 #else
-const char gnuplot_date[] = "2018-09-27 ";
+const char gnuplot_date[] = "2018-10-06 ";
 #endif
 const char gnuplot_copyright[] = "Copyright (C) 1986-1993, 1998, 2004, 2007-2018";
 
