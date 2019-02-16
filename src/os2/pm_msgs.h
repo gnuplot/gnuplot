74a9b764951992a52a0cb1bc5ef7f8a0de9cc679
Bastian Maerkisch <bmaerkisch@web.de>
2019-02-16 11:30:20 +0100

    pm: dashtype support
    Support for a dashed-line-mode was already available.  We add a PM_dashtype()
    terminal function and two new codes for the driver: one for dashed lines and
    one to set the color according to a terminal linetype.
    Remove the option to disable dashed lines in gnupmdrv.

--- src/os2/pm_msgs.h
+++ src/os2/pm_msgs.h
@@ -46,6 +46,7 @@
 #define SET_GRAPHICS	'G'
 #define SET_TEXT	'E'
 #define SET_LINE	'L'
+#define SET_DASH	'd'
 #define SET_FILLBOX	'B'
 #define SET_LINEWIDTH	'W'
 #define SET_ANGLE	'A'
@@ -57,7 +58,7 @@
 #define SET_SPECIAL	'o'  /* used for special options */
 #define SET_MENU	'#'
 #define GR_QUERY	'Q'
-#define GR_SUSPEND	'E'	/*'s' */
+#define GR_SUSPEND	'E'	/* 's' */
 #define GR_RESUME	'r'
 #define GR_MOVE		'M'
 #define GR_DRAW		'V'
@@ -75,6 +76,7 @@
 #define GR_RELEASE_PALETTE	'e'
 #define GR_SET_COLOR		'C'
 #define GR_SET_RGBCOLOR		'b'
+#define GR_LTCOLOR		'N'
 #define GR_FILLED_POLYGON	'f'
 #define GR_IMAGE		'i'
 #define GR_RGB_IMAGE	'I'
