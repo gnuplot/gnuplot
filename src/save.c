gnuplot/gnuplot

commit 3df39e8c5580bb9a22467109a327379a7ab9de15

revise space allocated for ylabel
Version 5.2 introduced an attempt to use the estimated character length of
the ylabel to decide the amount of space reserved to the left of the plot.
This caused multiple problems, exacerbated by the lack of options to
change the text justification.  The worst case was a non-rotated label
with TeX markup that was misinterpreted as printable text (Bug #2165).
These problems also applied to space on the right allocated for y2label.
Fixes:
 - use a fixed estimate of 3 character widths for nonrotated ylabel.
   The user can adjust the for longer labels using a combination of
   'set lmargin' and 'set ylabel offset delta_x'
 - parse and apply test justification left/right/center for ylabel
   The default remains center.
This returns the overall behavior to close to that of gnuplot 5.0
except that honoring left/right justification of ylabel text is new.

(cherry picked from commit 06f8367ec327a3ffe8e583c0616c77788fa69fcb)

src/save.c
@@ -788,6 +788,7 @@ set origin %g,%g\n",
 	save_position(fp, &(lab.offset), 3, TRUE);			 \
 	fprintf(fp, " font \"%s\"", lab.font ? conv_text(lab.font) : "");\
 	save_textcolor(fp, &(lab.textcolor));				 \
+	if (lab.pos != CENTRE) save_justification(lab.pos,fp);		 \
 	if (lab.tag == ROTATE_IN_3D_LABEL_TAG)				 \
 	    fprintf(fp, " rotate parallel");				 \
 	else if (lab.rotate == TEXT_VERTICAL)				 \

docs/gnuplot.doc
@@ -13838,6 +13838,8 @@ Ffigure_multiple_keys
 
  ylabel:  The y-axis label is centered to the left of the plot, defaulting to
  either horizontal or vertical orientation depending on the terminal type.
+ The program may not reserve enough space to the left of the plot to hold long
+ non-rotated ylabel text.  You can adjust this with `set lmargin`.
 
  zlabel: The z-axis label is centered along the z axis and placed in the space
  above the grid level.

src/boundary.c
@@ -1,7 +1,3 @@
-/*
- * $Id: boundary.c,v 1.50 2017/03/09 07:10:52 sfeam Exp $
- */
-
 /* GNUPLOT - boundary.c */
 
 /*[
@@ -247,8 +243,12 @@ boundary(struct curve_points *plots, int count)
 	    if (!axis_array[FIRST_Y_AXIS].ticmode)
 		ylabel_textwidth += 0.5 * t->v_char;
 	} else {
-	    /* How on earth did this used to work when it was always set to 0? */
-	    ylabel_textwidth = estimate_strlen(axis_array[FIRST_Y_AXIS].label.text) * t->h_char;
+	    /* Trying to estimate this length caused more problems than it solved.
+	     * For one thing it comes out wrong for text passed to TeX terminals.
+	     * Assume the common case is roughly 3 character widths and let the
+	     * user adjust lmargin and offset for longer non-rotated ylabels.
+	     */
+	    ylabel_textwidth = 3 * t->h_char;
 	}
     }
 
@@ -259,9 +259,8 @@ boundary(struct curve_points *plots, int count)
 	    if (!axis_array[SECOND_Y_AXIS].ticmode)
 		y2label_textwidth += 0.5 * t->v_char;
 	} else {
-	    /* should be (estimate_strlen()*t->h_char) but that's not what 5.0 did */
-	    /* this is an ugly ad hoc approximation of what it did before */
-	    y2label_textwidth = estimate_strlen(axis_array[SECOND_Y_AXIS].label.text) * t->h_char;
+	    /* See above. Estimating true text length causes problems */
+	    y2label_textwidth = 3 * t->h_char;
 	}
     }
 
@@ -751,9 +750,10 @@ boundary(struct curve_points *plots, int count)
 	+ ((float)xlablin+0.2) * t->v_char;
     xlabel_y -= ttic_textheight;
     ylabel_x = plot_bounds.xleft - ytic_width - ytic_textwidth;
-    ylabel_x -= ylabel_textwidth;
+    ylabel_x -= ylabel_textwidth/2;
 
     y2label_x = plot_bounds.xright + y2tic_width + y2tic_textwidth;
+    y2label_x += y2label_textwidth/2;
 
     /* Nov 2016  - simplify placement of timestamp
      * Stamp the same place on the page regardless of plot margins
@@ -1425,21 +1425,14 @@ draw_titles()
 	unsigned int x = ylabel_x;
 	unsigned int y = (plot_bounds.ytop + plot_bounds.ybot) / 2;
 	x += t->h_char;
-	axis_array[FIRST_Y_AXIS].label.pos = CENTRE;
 	write_label(x, y, &(axis_array[FIRST_Y_AXIS].label));
 	reset_textcolor(&(axis_array[FIRST_Y_AXIS].label.textcolor));
     }
 
     /* Y2LABEL */
     if (axis_array[SECOND_Y_AXIS].label.text) {
-	unsigned int x = y2label_x;
-	unsigned int y = (plot_bounds.ytop + plot_bounds.ybot) / 2;
-	if (axis_array[SECOND_Y_AXIS].label.rotate) {
-	    x += 2 * t->h_char;
-	    axis_array[SECOND_Y_AXIS].label.pos = CENTRE;
-	} else {
-	    axis_array[SECOND_Y_AXIS].label.pos = LEFT;
-	}
+	int x = y2label_x;
+	int y = (plot_bounds.ytop + plot_bounds.ybot) / 2;
 	write_label(x, y, &(axis_array[SECOND_Y_AXIS].label));
 	reset_textcolor(&(axis_array[SECOND_Y_AXIS].label.textcolor));
     }

src/show.c
@@ -2952,6 +2952,9 @@ show_xyzlabel(const char *name, const char *suffix, text_label *label)
 	fprintf(stderr, "\t%s%s is \"%s\", offset at ", name, suffix,
 	    label->text ? conv_text(label->text) : "");
 	show_position(&label->offset, 3);
+	fprintf(stderr, label->pos == LEFT ? " left justified"
+			: label->pos == RIGHT ? " right justified"
+			: "");
     } else
 	return;
 

commit 57dd6adf500814cabec6bfb61d09e022b1eb6884

must not save a justification for timestamp
Commit 3df39e8c inadvertently caused the line "set timestamp ..."
produced by "save" to include a justification setting.  But the
timestamp command does not accept a justification so this made the
save file produce an error when read.
Bug #2178

(cherry picked from commit a6ce6e78912256ee978469f41020c88348c5a97d)

src/save.c
@@ -780,7 +780,7 @@ set origin %g,%g\n",
     for (axis=0; axis<num_parallel_axes; axis++)
 	save_tics(fp, &parallel_axis[axis]);
 
-#define SAVE_AXISLABEL_OR_TITLE(name,suffix,lab)			 \
+#define SAVE_AXISLABEL_OR_TITLE(name,suffix,lab,savejust)		 \
     {									 \
 	fprintf(fp, "set %s%s \"%s\" ",					 \
 		name, suffix, lab.text ? conv_text(lab.text) : "");	 \
@@ -788,7 +788,7 @@ set origin %g,%g\n",
 	save_position(fp, &(lab.offset), 3, TRUE);			 \
 	fprintf(fp, " font \"%s\"", lab.font ? conv_text(lab.font) : "");\
 	save_textcolor(fp, &(lab.textcolor));				 \
-	if (lab.pos != CENTRE) save_justification(lab.pos,fp);		 \
+	if (savejust && (lab.pos != CENTRE)) save_justification(lab.pos,fp); \
 	if (lab.tag == ROTATE_IN_3D_LABEL_TAG)				 \
 	    fprintf(fp, " rotate parallel");				 \
 	else if (lab.rotate == TEXT_VERTICAL)				 \
@@ -800,11 +800,11 @@ set origin %g,%g\n",
 	fprintf(fp, "%s\n", (lab.noenhanced) ? " noenhanced" : "");	 \
     }
 
-    SAVE_AXISLABEL_OR_TITLE("", "title", title);
+    SAVE_AXISLABEL_OR_TITLE("", "title", title, TRUE);
 
     /* FIXME */
     fprintf(fp, "set timestamp %s \n", timelabel_bottom ? "bottom" : "top");
-    SAVE_AXISLABEL_OR_TITLE("", "timestamp", timelabel);
+    SAVE_AXISLABEL_OR_TITLE("", "timestamp", timelabel, FALSE);
 
     save_prange(fp, axis_array + T_AXIS);
     save_prange(fp, axis_array + U_AXIS);
@@ -812,7 +812,7 @@ set origin %g,%g\n",
 
 #define SAVE_AXISLABEL(axis)					\
     SAVE_AXISLABEL_OR_TITLE(axis_name(axis),"label",	\
-			    axis_array[axis].label)
+			    axis_array[axis].label, TRUE)
 
     SAVE_AXISLABEL(FIRST_X_AXIS);
     SAVE_AXISLABEL(SECOND_X_AXIS);