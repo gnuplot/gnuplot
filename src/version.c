dd73aeb3fbcb60bc5788339aff31928d0ad241b9
Ethan A Merritt <merritt@u.washington.edu>
2019-06-12 13:01:56 -0700

    Bump version to 8alpha
    internal use; post-5.2.7 but not yet a release candidate for 5.2.8

--- src/version.c
+++ src/version.c
@@ -36,11 +36,11 @@
 
 
 const char gnuplot_version[] = "5.2";
-const char gnuplot_patchlevel[] = "7";
+const char gnuplot_patchlevel[] = "8alpha";
 #ifdef DEVELOPMENT_VERSION
 #include "timestamp.h"
 #else
-const char gnuplot_date[] = "2019-05-29 ";
+const char gnuplot_date[] = "2019-06-12 ";
 #endif
 const char gnuplot_copyright[] = "Copyright (C) 1986-1993, 1998, 2004, 2007-2019";
 
