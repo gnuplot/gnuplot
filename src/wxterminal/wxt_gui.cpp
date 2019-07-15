7675ee27917b3bdec802222f31953c28dd7b0e9e
Ethan A Merritt <merritt@u.washington.edu>
2019-07-14 22:34:09 -0700

    wxt: disable ExportToFile widget in persist mode
    For mono-threaded wxWidgets, activating this widget after the persist
    code forks can cause the program to hang waiting for terminal input.
    Bug #2185
    
    (cherry picked from commit f193a18dabfaee4fe6b5771007c9f5b58edbfed8)

--- src/wxterminal/wxt_gui.cpp
+++ src/wxterminal/wxt_gui.cpp
@@ -4199,6 +4199,7 @@ void wxt_atexit()
 			wxt_iter->frame->toolbar->EnableTool(Toolbar_ZoomPrevious, false);
 			wxt_iter->frame->toolbar->EnableTool(Toolbar_ZoomNext, false);
 			wxt_iter->frame->toolbar->EnableTool(Toolbar_Autoscale, false);
+			wxt_iter->frame->toolbar->EnableTool(Toolbar_ExportToFile, false);
 		} else {
 			wxt_iter->frame->Destroy();
 		}
