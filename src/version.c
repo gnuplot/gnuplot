c58d70cd40830f2e37a8e6e535717fbb06770bc9
Ethan A Merritt <merritt@u.washington.edu>
2019-09-09 14:02:06 -0700

    Bump version to 5.2.8beta for testing

--- src/version.c
+++ src/version.c
@@ -36,11 +36,11 @@
 
 
 const char gnuplot_version[] = "5.2";
-const char gnuplot_patchlevel[] = "8alpha";
+const char gnuplot_patchlevel[] = "8beta";
 #ifdef DEVELOPMENT_VERSION
 #include "timestamp.h"
 #else
-const char gnuplot_date[] = "2019-06-12 ";
+const char gnuplot_date[] = "2019-09-05 ";
 #endif
 const char gnuplot_copyright[] = "Copyright (C) 1986-1993, 1998, 2004, 2007-2019";
 
