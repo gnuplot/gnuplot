13cbece9cf23d48a9026ea2bfcf1ade82d5865f1
Ethan A Merritt <merritt@u.washington.edu>
2020-01-31 14:05:33 -0800

    xdg: Use C-style comments

--- src/xdg.c
+++ src/xdg.c
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-/// Names of the environment variables, mapped to XDGVarType values
+/* Names of the environment variables, mapped to XDGVarType values */
 static const char *xdg_env_vars[] = {
     [kXDGConfigHome] = "XDG_CONFIG_HOME",
     [kXDGDataHome] = "XDG_DATA_HOME",
@@ -19,9 +19,9 @@ static const char *xdg_env_vars[] = {
     [kXDGDataDirs] = "XDG_DATA_DIRS",
 };
 
-/// Defaults for XDGVarType values
-///
-/// Used in case environment variables contain nothing. Need to be expanded.
+/* Defaults for XDGVarType values
+ * Used in case environment variables contain nothing. Need to be expanded.
+ */
 static const char *const xdg_defaults[] = {
     [kXDGConfigHome] = "~/.config",
     [kXDGDataHome] = "~/.local/share",
@@ -32,27 +32,29 @@ static const char *const xdg_defaults[] = {
 };
 
 
-/// Return XDG variable value
-///
-/// First query the environement variable and if that does not exist fall back
-/// to the defaults. Further reading:
-/// https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
-///
-/// @param[in]  idx  XDG variable to use.
-///
-/// @return [allocated] variable value.
+/* Return XDG variable value
+ *
+ * First query the environement variable and if that does not exist fall back
+ * to the defaults. Further reading:
+ * https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+ *
+ * @param[in]  idx  XDG variable to use.
+ *
+ * @return [allocated] variable value.
+ */
 char *xdg_get_var(const XDGVarType idx) {
-    // Check the environment variable, if it is there, we are done.
+    /* Check the environment variable. If it is there, we are done. */
     char *XDGVar;
     if ((XDGVar = getenv(xdg_env_vars[idx])) != NULL) {
         return gp_strdup(XDGVar);
     }
 
-    // Looks like the environment variable is not there.
-    // Load the default and run word expansion on it.
+    /* Looks like the environment variable is not there.
+     * Load the default and run word expansion on it.
+     */
     XDGVar = gp_strdup(xdg_defaults[idx]);
     gp_expand_tilde(&XDGVar);
     return XDGVar;
 }
 
-#endif // __unix__
+#endif /* __unix__ */
--- src/xdg.h
+++ src/xdg.h
@@ -3,20 +3,20 @@
 
 #ifdef __unix__
 
-/// List of possible XDG variables
+/* List of possible XDG variables */
 typedef enum {
   kXDGNone = -1,
-  kXDGConfigHome,  ///< XDG_CONFIG_HOME
-  kXDGDataHome,    ///< XDG_DATA_HOME
-  kXDGCacheHome,   ///< XDG_CACHE_HOME
-  kXDGRuntimeDir,  ///< XDG_RUNTIME_DIR
-  kXDGConfigDirs,  ///< XDG_CONFIG_DIRS
-  kXDGDataDirs,    ///< XDG_DATA_DIRS
+  kXDGConfigHome,  /* XDG_CONFIG_HOME */
+  kXDGDataHome,    /* XDG_DATA_HOME */
+  kXDGCacheHome,   /* XDG_CACHE_HOME */
+  kXDGRuntimeDir,  /* XDG_RUNTIME_DIR */
+  kXDGConfigDirs,  /* XDG_CONFIG_DIRS */
+  kXDGDataDirs,    /* XDG_DATA_DIRS */
 } XDGVarType;
 
 char *xdg_get_var(const XDGVarType idx);
 
-#endif // __unix__
+#endif /* __unix__ */
 
-#endif // GNUPLOT_XDG_H
+#endif /* GNUPLOT_XDG_H */
 
