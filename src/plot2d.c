46586c62abe7f3ca02d078cee62b183ba6c8fa52
Ethan A Merritt <merritt@u.washington.edu>
2020-03-09 12:47:22 -0700

    treat "plot with polygons" (2D) as "plot with filledcurves closed"

--- src/plot2d.c
+++ src/plot2d.c
@@ -1033,6 +1033,12 @@ get_data(struct curve_points *current_plot)
 	    break;
 	}
 
+	case POLYGONS:
+	{   /* Nothing yet to distinguish this from filledcurves */
+	    store2d_point(current_plot, i++, v[0], v[1], v[0], v[0], v[1], v[1], 0);
+	    break;
+	}
+
 	case BOXPLOT:
 	{   /* 2 columns:  x data
 	     * 3 columns:  x data width
@@ -1212,7 +1218,6 @@ get_data(struct curve_points *current_plot)
 	case SURFACEGRID:
 	case ZERRORFILL:
 	case ISOSURFACE:
-	case POLYGONS:
 	    int_error(NO_CARET, "This plot style only available for splot");
 	    break;
 
@@ -2842,6 +2847,11 @@ eval_plots()
 		axis_init(&parallel_axis_array[paxis_current-1], TRUE);
 		parallel_axis_array[paxis_current-1].paxis_x
 			= (paxis_x > -VERYLARGE) ? paxis_x : (double)paxis_current;
+	    }
+
+	    /* Currently polygons are just treated as filled curves */
+	    if (this_plot->plot_style == POLYGONS) {
+		this_plot->filledcurves_options.closeto = FILLEDCURVES_CLOSED;
 	    }
 
 	    /* Styles that use palette */
