ae1a979f9ad1d2d7fdc002a402f153e7af19a33a
Ethan A Merritt <merritt@u.washington.edu>
2020-03-21 13:14:09 -0700

    bump date for rc0 release

--- src/version.c
+++ src/version.c
@@ -40,7 +40,7 @@ const char gnuplot_patchlevel[] = "rc0";
 #ifdef DEVELOPMENT_VERSION
 #include "timestamp.h"
 #else
-const char gnuplot_date[] = "2020-03-03 ";
+const char gnuplot_date[] = "2020-03-21 ";
 #endif
 const char gnuplot_copyright[] = "Copyright (C) 1986-1993, 1998, 2004, 2007-2020";
 
