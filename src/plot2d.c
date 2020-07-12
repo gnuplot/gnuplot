gnuplot/gnuplot

commit 7390145fe50187ffde54668d22eb0b99091a64de

Accept "columnheader(<expression>)" as a synonym for "columnhead(<expression>)"
Adds no functionality but may reduce user confusion.

src/plot2d.c
@@ -3682,8 +3682,7 @@ parse_plot_title(struct curve_points *this_plot, char *xtitle, char *ytitle, TBO
 
 	/* This ugliness is because columnheader can be either a keyword */
 	/* or a function name.  Yes, the design could have been better. */
-	if (almost_equals(c_token,"col$umnheader")
-	&& !(equals(c_token,"columnhead") && equals(c_token+1,"(")) ) {
+	if (almost_equals(c_token,"col$umnheader") && !equals(c_token+1,"(")) {
 	    df_set_key_title_columnhead(this_plot);
 	} else if (equals(c_token,"at")) {
 	    *set_title = FALSE;

src/eval.c
@@ -141,6 +141,7 @@ const struct ft_entry ft[] =
     {"stringcolumn",  f_stringcolumn},	/* for using specs */
     {"strcol",  f_stringcolumn},	/* shorthand form */
     {"columnhead",  f_columnhead},
+    {"columnheader",  f_columnhead},
     {"valid",  f_valid},
     {"timecolumn",  f_timecolumn},
 

commit f9f0a275073a02a09bd622f2a8fccfebb1ea6a6d

fix regression caused by commit 7390145f
Accept "columnheader(<expression>)" as a synonym for "columnhead(<expression>)"

demo/datastrings.dem
@@ -1,9 +1,8 @@
 #
-# Demonstrate features of datastrings patch
+# Demonstrate reading strings from column headings or data column
+# for placement as plot titles or tic labels
 # Ethan A Merritt <merritt@u.washington.edu>
 # 08-Mar-2003
-#
-reset
 #
 # Illustrate reading plot labels from column head in datafile
 #
@@ -25,7 +24,7 @@ pause -1 "<cr> to plot again using x2ticlabels"
 # Illustrate reading xtic axis labels from the datafile
 #
 reset
-set title "Read tic labels from a datafile column\nAn approximation of Hans Olav Eggestad's categoric plot patch\nusing 'using ($0):2:xticlabels(1)' and 'set style fill solid border -1'"
+set title "Read tic labels from a datafile column\nusing 'using ($0):2:xticlabels(1)'"
 set boxwidth 0.3
 set style fill solid 1.000000 border -1
 set bmargin 3
@@ -55,10 +54,10 @@ plot 'ctg-y2.dat' using ($0-0.2):2 index 0 with boxes title "precipitation 1992-
 pause -1 "<cr> to plot same data from table format"
 set title "Plot from table format (titles taken from column headers)"
 unset x2tics
-plot 'table.dat' using ($0-0.2):2 title column(2) with boxes, \
-     ''          using ($0+0.2):3 title column(3) with boxes, \
-     ''          using ($0):4     title column(4) with linespoints, \
-     ''          using ($0):5:xtic(1) title column(5) with linespoints
+plot 'table.dat' using ($0-0.2):2 title columnhead(2) with boxes, \
+     ''          using ($0+0.2):3 title columnhead(3) with boxes, \
+     ''          using ($0):4     title columnhead(4) with linespoints, \
+     ''          using ($0):5:xtic(1) title columnhead(5) with linespoints
 #
 pause -1 "<cr> to show double use of y values"
 #

src/plot2d.c
@@ -3682,7 +3682,8 @@ parse_plot_title(struct curve_points *this_plot, char *xtitle, char *ytitle, TBO
 
 	/* This ugliness is because columnheader can be either a keyword */
 	/* or a function name.  Yes, the design could have been better. */
-	if (almost_equals(c_token,"col$umnheader") && !equals(c_token+1,"(")) {
+	if (almost_equals(c_token,"col$umnheader")
+	&& !(almost_equals(c_token,"columnhead$er") && equals(c_token+1,"(")) ) {
 	    df_set_key_title_columnhead(this_plot);
 	} else if (equals(c_token,"at")) {
 	    *set_title = FALSE;