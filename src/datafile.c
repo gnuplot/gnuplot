gnuplot/gnuplot

commit b94024a80a355b2c64078c81016dcc48acad3457

"stats <non-existent filename>" yields a testable, non-fatal error
The motivation is to allow a test-and-plot sequence as in this example

    do for [i=first:last] {
	filename = sprintf("file%02d.dat", i)
	stats filename nooutput
	if (GPVAL_ERRNO) {
	    print GPVAL_ERRMSG
	    continue
	}
	plot filename title filename
    }

src/datafile.c
@@ -1399,19 +1399,35 @@ df_open(const char *cmd_filename, int max_using, struct curve_points *plot)
 
 	/* filename cannot be static array! */
 	gp_expand_tilde(&df_filename);
+
+	/* Open failure generates a warning rather than an immediate fatal error.
+	 * We assume success (GPVAL_ERRNO == 0) and let the caller change this to
+	 * something else if it considers DF_EOF a serious error.
+	 */
+	fill_gpval_integer("GPVAL_ERRNO", 0);
+
 #ifdef HAVE_SYS_STAT_H
 	{
 	    struct stat statbuf;
 	    if ((stat(df_filename, &statbuf) > -1) &&
 		S_ISDIR(statbuf.st_mode)) {
-		os_error(name_token, "\"%s\" is a directory",
-			df_filename);
+		char *errmsg = gp_alloc(32 + strlen(df_filename), "errmsg");
+		sprintf(errmsg, "\"%s\" is a directory", df_filename);
+		int_warn(name_token, errmsg);
+		fill_gpval_string("GPVAL_ERRMSG", errmsg);
+		free(errmsg);
+		df_eof = 1;
+		return DF_EOF;
 	    }
 	}
 #endif /* HAVE_SYS_STAT_H */
 
 	if ((data_fp = loadpath_fopen(df_filename, df_binary_file ? "rb" : "r")) == NULL) {
-	    int_warn(NO_CARET, "Cannot find or open file \"%s\"", df_filename);
+	    char *errmsg = gp_alloc(32 + strlen(df_filename), "errmsg");
+	    sprintf(errmsg, "Cannot find or open file \"%s\"", df_filename);
+	    int_warn(NO_CARET, errmsg);
+	    fill_gpval_string("GPVAL_ERRMSG", errmsg);
+	    free(errmsg);
 	    df_eof = 1;
 	    return DF_EOF;
 	}

src/stats.c
@@ -896,7 +896,8 @@ statsrequest(void)
 	columns = df_open(file_name, 2, NULL);	/* up to 2 using specs allowed */
 
 	if (columns < 0) {
-	    int_warn(NO_CARET, "Can't read data file");
+	    /* This allows the user to test for failure */
+	    fill_gpval_integer("GPVAL_ERRNO", 1);
 	    while (!END_OF_COMMAND)
 		c_token++;
 	    goto stats_cleanup;

commit 97c16c99593ff2516faee0821c2fc8155a1ad892

must not pass user-supplied string as 1st parameter to vfprintf
Spurious % characters will cause illegal access to additional parameters
that do not actually exist.
The error at this call site was introduced by commit b94024a80a.

Bug #2320

src/datafile.c
@@ -1423,9 +1423,9 @@ df_open(const char *cmd_filename, int max_using, struct curve_points *plot)
 		S_ISDIR(statbuf.st_mode)) {
 		char *errmsg = gp_alloc(32 + strlen(df_filename), "errmsg");
 		sprintf(errmsg, "\"%s\" is a directory", df_filename);
-		int_warn(name_token, errmsg);
 		fill_gpval_string("GPVAL_ERRMSG", errmsg);
 		free(errmsg);
+		int_warn(name_token, "\"%s\" is a directory", df_filename);
 		df_eof = 1;
 		return DF_EOF;
 	    }
@@ -1435,9 +1435,9 @@ df_open(const char *cmd_filename, int max_using, struct curve_points *plot)
 	if ((data_fp = loadpath_fopen(df_filename, df_binary_file ? "rb" : "r")) == NULL) {
 	    char *errmsg = gp_alloc(32 + strlen(df_filename), "errmsg");
 	    sprintf(errmsg, "Cannot find or open file \"%s\"", df_filename);
-	    int_warn(NO_CARET, errmsg);
 	    fill_gpval_string("GPVAL_ERRMSG", errmsg);
 	    free(errmsg);
+	    int_warn(NO_CARET, "Cannot find or open file \"%s\"", df_filename);
 	    df_eof = 1;
 	    return DF_EOF;
 	}