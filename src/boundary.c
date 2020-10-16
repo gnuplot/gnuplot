56c526838f51c51b3fb3860b0eaffda4ae4a05ad
Ethan A Merritt <merritt@u.washington.edu>
2020-10-16 12:26:48 -0700

    preserve key position across a singleton plot with "title $foo at x,y"
    Bug #2342

--- src/boundary.c
+++ src/boundary.c
@@ -1189,6 +1189,8 @@ do_key_sample(
     coordval var_color)
 {
     struct termentry *t = term;
+    int xl_save = xl;
+    int yl_save = yl;
 
     /* Clip key box against canvas */
     BoundingBox *clip_save = clip_area;
@@ -1350,6 +1352,8 @@ do_key_sample(
 
     /* Restore previous clipping area */
     clip_area = clip_save;
+    xl = xl_save;
+    yl = yl_save;
 }
 
 void
@@ -1358,6 +1362,8 @@ do_key_sample_point(
     legend_key *key)
 {
     struct termentry *t = term;
+    int xl_save = xl;
+    int yl_save = yl;
 
     /* If the plot this title belongs to specified a non-standard place
      * for the key sample to appear, use that to override xl, yl.
@@ -1412,6 +1418,8 @@ do_key_sample_point(
 	}
     }
 
+    xl = xl_save;
+    yl = yl_save;
     (t->layer)(TERM_LAYER_END_KEYSAMPLE);
 }
 
