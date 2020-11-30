1326d3bfad5863a425a050d0639ce553cdacdc07
Ethan A Merritt <merritt@u.washington.edu>
2020-11-29 22:26:07 -0800

    Bump version, date, release notes, etc for release of 5.4.1

--- src/version.c
+++ src/version.c
@@ -36,11 +36,11 @@
 
 
 const char gnuplot_version[] = "5.4";
-const char gnuplot_patchlevel[] = "0";
+const char gnuplot_patchlevel[] = "1";
 #ifdef DEVELOPMENT_VERSION
 #include "timestamp.h"
 #else
-const char gnuplot_date[] = "2020-07-13 ";
+const char gnuplot_date[] = "2020-12-01 ";
 #endif
 const char gnuplot_copyright[] = "Copyright (C) 1986-1993, 1998, 2004, 2007-2020";
 
