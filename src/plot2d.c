gnuplot/gnuplot

commit 19b68b9534b6e95807efe606b1a0bee0cc64ec7c

Change criterion for fallback to default axes in 'plot with ellipse'
plot ... using x y major minor [orientation] ... with ellipse

Old:	use default axes if both major = 0 or minor = 0
	otherwise abs(major), abs(minor)
New:	use default axes if either major < 0 or minor < 0
	otherwise major, minor

Under the old interpretation it was hard to specify a zero-size ellipse.

src/plot2d.c
@@ -1142,16 +1142,16 @@ get_data(struct curve_points *current_plot)
 
 	case ELLIPSES:
 	{   /* x y
-	     * x y major_diam
+	     * x y diam  (used for both major and minor axis)
 	     * x y major_diam minor_diam
 	     * x y major_diam minor_diam orientation
 	     */
 	    coordval x = v[0];
 	    coordval y = v[1];
-	    coordval major_axis = (j >= 3) ? fabs(v[2]) : 0.0;
-	    coordval minor_axis = (j >= 4) ? fabs(v[3]) : (j >= 3) ? fabs(v[2]) : 0.0;
+	    coordval major_axis = (j >= 3) ? v[2] : 0.0;
+	    coordval minor_axis = (j >= 4) ? v[3] : (j >= 3) ? v[2] : 0.0;
 	    coordval orientation = (j >= 5) ? v[4] : 0.0;
-	    coordval flag = (major_axis > 0 && minor_axis > 0) ? 0.0 : DEFAULT_RADIUS;
+	    coordval flag = (major_axis < 0 || minor_axis < 0) ?  DEFAULT_RADIUS : 0;
 
 	    if (j == 2)	/* FIXME: why not also for j == 3 or 4? */
 		orientation = default_ellipse.o.ellipse.orientation;

docs/gnuplot.doc
@@ -4131,7 +4131,7 @@ Ffigure_ellipses
  and the x axis.
 
       2 columns: x y
-      3 columns: x y major_diam
+      3 columns: x y diam  (used for both major and minor axes)
       4 columns: x y major_diam minor_diam
       5 columns: x y major_diam minor_diam angle
 
@@ -4140,10 +4140,15 @@ Ffigure_ellipses
  (see `set style ellipse`).  The orientation of the ellipse, which is
  defined as the angle between the major diameter and the plot's x axis,
  is taken from the default ellipse style (see `set style ellipse`).
+
  If three input columns are provided, the third column is used for both
  diameters.  The orientation angle defaults to zero.
+
  If four columns are present, they are interpreted as x, y, major diameter,
  minor diameter.  Note that these are diameters, not radii.
+ If either diameter is negative, both diameters will be taken from the
+ default set by `set style ellipse`.
+
  An optional 5th column may specify the orientation angle in degrees.
  The ellipses will also be drawn with their default extent if either of the
  supplied diameters in the 3-4-5 column form is negative.

src/graphics.c
@@ -2530,46 +2530,42 @@ plot_ellipses(struct curve_points *plot)
 	    e->center.y = map_y(plot->points[i].y);
 	    if (invalid_coordinate(e->center.x, e->center.y))
 		continue;
-
-	    e->extent.x = plot->points[i].xlow; /* major axis */
-	    e->extent.y = plot->points[i].xhigh; /* minor axis */
-	    /* the mapping can be set by the
-	     * "set ellipseaxes" setting
-	     * both x units, mixed, both y units */
-	    /* clumsy solution */
-	    switch (e->type) {
-	    case ELLIPSEAXES_XY:
-		map_position_r(&e->extent, &tempx, &tempy, "ellipse");
-		e->extent.x = tempx;
-		e->extent.y = tempy;
-		break;
-	    case ELLIPSEAXES_XX:
-		map_position_r(&e->extent, &tempx, &tempy, "ellipse");
-		tempfoo = tempx;
-		e->extent.x = e->extent.y;
-		map_position_r(&e->extent, &tempy, &tempx, "ellipse");
-		e->extent.x = tempfoo;
-		e->extent.y = tempy;
-		break;
-	    case ELLIPSEAXES_YY:
-		map_position_r(&e->extent, &tempx, &tempy, "ellipse");
-		tempfoo = tempy;
-		e->extent.y = e->extent.x;
-		map_position_r(&e->extent, &tempy, &tempx, "ellipse");
-		e->extent.x = tempx;
-		e->extent.y = tempfoo;
-		break;
-	    }
+	    e->orientation = plot->points[i].ylow;
 
 	    if (plot->points[i].z <= DEFAULT_RADIUS) {
 		map_position_r(&default_ellipse.o.ellipse.extent,
 				&e->extent.x, &e->extent.y, "ellipse");
+	    } else {
+		e->extent.x = plot->points[i].xlow; /* major axis */
+		e->extent.y = plot->points[i].xhigh; /* minor axis */
+		/* the mapping can be set by the
+		 * "set ellipseaxes" setting
+		 * both x units, mixed, both y units */
+		/* clumsy solution */
+		switch (e->type) {
+		case ELLIPSEAXES_XY:
+		    map_position_r(&e->extent, &tempx, &tempy, "ellipse");
+		    e->extent.x = tempx;
+		    e->extent.y = tempy;
+		    break;
+		case ELLIPSEAXES_XX:
+		    map_position_r(&e->extent, &tempx, &tempy, "ellipse");
+		    tempfoo = tempx;
+		    e->extent.x = e->extent.y;
+		    map_position_r(&e->extent, &tempy, &tempx, "ellipse");
+		    e->extent.x = tempfoo;
+		    e->extent.y = tempy;
+		    break;
+		case ELLIPSEAXES_YY:
+		    map_position_r(&e->extent, &tempx, &tempy, "ellipse");
+		    tempfoo = tempy;
+		    e->extent.y = e->extent.x;
+		    map_position_r(&e->extent, &tempy, &tempx, "ellipse");
+		    e->extent.x = tempx;
+		    e->extent.y = tempfoo;
+		    break;
+		}
 	    }
-
-	    /* May 2018 - default orientation used to be signalled by
-	     * DEFAULT_ELLIPSE rather than passed in explicitly
-	     */
-	    e->orientation = plot->points[i].ylow;
 
 	    /* rgb variable  -  color read from data column */
 	    if (!check_for_variable_color(plot, &plot->varcolor[i]) && withborder)

src/set.c
@@ -4935,6 +4935,10 @@ set_style()
 	    if (equals(c_token,"size")) {
 		c_token++;
 		get_position(&default_ellipse.o.ellipse.extent);
+		if (default_ellipse.o.ellipse.extent.x < 0)
+		    default_ellipse.o.ellipse.extent.x = 0;
+		if (default_ellipse.o.ellipse.extent.y < 0)
+		    default_ellipse.o.ellipse.extent.y = 0;
 		c_token--;
 	    } else if (almost_equals(c_token,"ang$le")) {
 		c_token++;
@@ -5549,6 +5553,7 @@ set_view()
 	}
 	return;
     } else if (almost_equals(c_token,"noequal$_axes")) {
+	/* FIXME: set aspect_ratio = 0 ?? */
 	aspect_ratio_3D = 0;
 	c_token++;
 	return;

commit 2ef3f2803fcce63e5af58e5d5a254c790e61e9fc

"with ellipses" 2-column input failed to use default size from 'set style ellipse'
Bug introduced by commit 19b68b95

src/plot2d.c
@@ -1164,7 +1164,7 @@ get_data(struct curve_points *current_plot)
 	    coordval major_axis = (j >= 3) ? v[2] : 0.0;
 	    coordval minor_axis = (j >= 4) ? v[3] : (j >= 3) ? v[2] : 0.0;
 	    coordval orientation = (j >= 5) ? v[4] : 0.0;
-	    coordval flag = (major_axis < 0 || minor_axis < 0) ?  DEFAULT_RADIUS : 0;
+	    coordval flag = (major_axis <= 0 || minor_axis <= 0) ?  DEFAULT_RADIUS : 0;
 
 	    if (j == 2)	/* FIXME: why not also for j == 3 or 4? */
 		orientation = default_ellipse.o.ellipse.orientation;