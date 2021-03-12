2bbeda06a1cb32f5ccc355d8bb1fc3bbfa4db2fa
Ethan A Merritt <merritt@u.washington.edu>
2021-03-12 11:55:27 -0800

    contour plots "with labels": do not store useless blank label for every data point

--- src/plot3d.c
+++ src/plot3d.c
@@ -1412,8 +1412,10 @@ get_3ddata(struct surface_points *this_plot)
 	    /* At this point we have stored the point coordinates. Now we need to copy */
 	    /* x,y,z into the text_label structure and add the actual text string.     */
 	    if (this_plot->plot_style == LABELPOINTS) {
-		/* Aug 2018: only store INRANGE labels. This is a CHANGE. */
-		if (cp->type == INRANGE)
+		if (draw_contour && !this_plot->opt_out_of_contours)
+		    /* Contour labels are calculated and added later */
+		    ;
+		else if (cp->type == INRANGE)
 		    store_label(this_plot->labels, cp, xdatum, df_tokens[3], color);
 	    }
 
