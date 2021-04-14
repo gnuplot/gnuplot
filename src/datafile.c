gnuplot/gnuplot

commit e96f1b9493c4b7bef6bd612f68af01543a2dcb86

defer evaluation of "plot ... title <expression>" until after data is read in

src/datafile.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: datafile.c,v 1.353 2017/09/05 20:18:58 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: datafile.c,v 1.354 2017/09/15 18:35:57 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - datafile.c */
@@ -296,7 +296,9 @@ static TBOOLEAN df_tabulate_strings = FALSE;	/* used only by TABLESTYLE */
 #define NO_COLUMN_HEADER (-99)  /* some value that can never be a real column */
 static int column_for_key_title = NO_COLUMN_HEADER;
 static TBOOLEAN df_already_got_headers = FALSE;
-char *df_key_title = NULL;     /* filled in from column header if requested */
+
+char *df_key_title = NULL;		/* filled in from column header if requested */
+struct at_type *df_plot_title_at;	/* used for deferred evaluation of plot title */
 
 
 /* Binary *read* variables used by df_readbinary().
@@ -2628,7 +2630,6 @@ f_stringcolumn(union argument *arg)
 void
 f_columnhead(union argument *arg)
 {
-    static char placeholder[] = "@COLUMNHEAD0000@";
     struct value a;
 
     if (!evaluate_inside_using)
@@ -2637,29 +2638,38 @@ f_columnhead(union argument *arg)
     (void) arg;                 /* avoid -Wunused warning */
     (void) pop(&a);
     column_for_key_title = (int) real(&a);
+
+    /* This handles the case:    plot ... using (column("FOO")) ... title columnhead
+     * FIXME: Why can't create_call_columnhead() dummy up the real column
+     *        rather than loading -1 and requiring that we retrieve via df_key_title?
+     */
+    if (column_for_key_title == -1) {
+	push(Gstring(&a, df_key_title));
+	return;
+    }
+
     if (column_for_key_title < 0 || column_for_key_title > 9999)
 	column_for_key_title = 0;
-    snprintf(placeholder+11, 6, "%4d@", column_for_key_title);
 
-    /* The program could be in either of two states, depending on where
-     * the call to columnheader(N) appeared. 
+    /* Sep 2017 - It used to be that the program could be in either of two states,
+     * depending on where the call to columnheader(N) appeared. 
      * 1) called from a using spec;  we already read the header line
+     *    This means that df_column points to a structure containing column headers
+     * 2) called from 'title columnheader(N)'; we have not yet read the header line
+     *    and df_column == NULL
+     * Now we defer evaluation of the title via df_title_at, so case 2 never happens.
      */
     if (df_column) {
 	if ((0 < column_for_key_title && column_for_key_title <= df_max_cols)
-	&&  (df_column && df_column[column_for_key_title-1].header))
+	&&  (df_column[column_for_key_title-1].header))
 	    push(Gstring(&a, df_column[column_for_key_title-1].header));
-	else
-	    push(Gstring(&a, placeholder));
+	else {
+	    static char unknown_column[2] = {0,0};
+	    push(Gstring(&a, &unknown_column[0]));
+	}
     } else {
-
-    /* 2) called from 'title columnheader(N)'
-     *    We have not yet read from the file so we don't know the actual header.
-     *    Instead we return a placeholder that will be expanded later
-     */
-	push(Gstring(&a, placeholder));
+/* DEBUG */ int_error(NO_CARET,"Internal error: df_column[] not initialized\n");
     }
-
 }
 
 
@@ -2839,7 +2849,7 @@ expect_string(const char column)
 }
 
 /*
- * Load plot title for key box from the string found earlier by df_readline.
+ * Load plot title used in key box from the string found earlier by df_readline.
  * Called from get_data().
  */
 void
@@ -2861,35 +2871,28 @@ df_set_key_title(struct curve_points *plot)
     }
 
     /* What if there was already a title specified? */
-    if (plot->title && !plot->title_is_filename) {
-	int columnhead;
-	char *placeholder = strstr(plot->title, "@COLUMNHEAD");
-
-	while (placeholder) {
-	    char *newtitle = gp_alloc(strlen(plot->title) + df_longest_columnhead, "plot title");
-	    char *trailer = NULL;
-	    columnhead = strtol(placeholder+11, &trailer, 0);
-	    *placeholder = '\0';
-	    if (trailer && *trailer == '@')
-		trailer++;
-	    sprintf(newtitle, "%s%s%s", plot->title,
-		    (columnhead <= 0) ? df_key_title :
-		    (columnhead <= df_no_cols) ? df_column[columnhead-1].header : "",
-		    trailer ? trailer : "");
+    if (df_plot_title_at) {
+	struct value a;
+	evaluate_inside_using = TRUE;
+	evaluate_at(df_plot_title_at, &a);
+	evaluate_inside_using = FALSE;
+	if (a.type == STRING) {
 	    free(plot->title);
-	    plot->title = newtitle;
-	    placeholder = strstr(newtitle, "@COLUMNHEAD");
-	}
+	    plot->title = a.v.string_val;
+	} else
+	    int_warn(NO_CARET, "\ttitle did not evaluate to a string!?\n");
 	return;
     }
+
+    /* "notitle <whatever-the-title-would-have-been>" */
     if (plot->title_is_suppressed)
 	return;
-    if (plot->title)
-	free(plot->title);
 
-    plot->title_no_enhanced = !keyT.enhanced;
+    /* Note: I think we can only get here for histogram labels */
+    free(plot->title);
     plot->title = df_key_title;
     df_key_title = NULL;
+    plot->title_no_enhanced = !keyT.enhanced;
 }
 
 /*
@@ -2917,8 +2920,16 @@ df_set_key_title_columnhead(struct curve_points *plot)
 	    column_for_key_title = use_spec[1].column;
     }
     /* This results from  plot 'foo' using (column("name")) title columnhead */
-    if (column_for_key_title == NO_COLUMN_HEADER)
-	plot->title = gp_strdup("@COLUMNHEAD-1@");
+    /* FIXME:  builds on top of older circuitous method
+     * - here we dummy up a call to columnhead(-1)
+     * - somewhere else the real columnhead is stored in df_key_title
+     * - when columnhead(-1) is evaluated, it returns the content of df_key_title
+     * Why can't we dummy up columhead(real column) and skip the intermediate step?
+     */
+    if (column_for_key_title == NO_COLUMN_HEADER) {
+	free_at(df_plot_title_at);
+	df_plot_title_at = create_call_columnhead();
+    }
 }
 
 char *

demo/html/Makefile
@@ -12,7 +12,7 @@ GNUPLOT_LIB = ..
 # Several of the demos need manual intervention:
 #	fontfile vector epslatex
 #
-html: airfoil.html approximate.html armillary.html array.html arrowstyle.html barchart_art.html binary.html bins.html bivariat.html boxplot.html boxclusters.html candlesticks.html cerf.html circles.html cities.html complex_trig.html contours.html controls.html custom_key.html dashtypes.html datastrings.html dgrid3d.html discrete.html electron.html ellipse.html enhanced_utf8.html epslatex.html errorbars.html fenceplot.html fillbetween.html fillcrvs.html fillstyle.html finance.html fit.html hidden.html hidden2.html histograms.html histograms2.html gantt.html image.html image2.html index.html iterate.html jitter.html layout.html lines_arrows.html linkedaxes.html margins.html monotonic_spline.html multiaxis.html multimsh.html multiplt.html nokey.html nonlinear1.html nonlinear3.html parallel.html param.html piecewise.html pm3dcolors.html pm3d.html pm3dgamma.html pm3d_lighting.html pointsize.html polar.html poldat.html prob2.html prob.html rainbow.html random.html rectangle.html rgba_lines.html rgbalpha.html rgb_variable.html rotate_labels.html rugplot.html running_avg.html scatter.html simple.html singulr.html smooth.html solar_path.html spline.html smooth_splines.html steps.html stringvar.html surface1.html surface2.html azimuth.html transparent.html transparent_solids.html textbox.html tics.html timedat.html ttics.html using.html varcolor.html vector.html violinplot.html world.html world2.html animate2.html heatmaps.html stats.html zerror.html
+html: airfoil.html approximate.html armillary.html array.html arrowstyle.html barchart_art.html binary.html bins.html bivariat.html boxplot.html boxclusters.html candlesticks.html cerf.html circles.html cities.html complex_trig.html columnhead.html contours.html controls.html custom_key.html dashtypes.html datastrings.html dgrid3d.html discrete.html electron.html ellipse.html enhanced_utf8.html epslatex.html errorbars.html fenceplot.html fillbetween.html fillcrvs.html fillstyle.html finance.html fit.html hidden.html hidden2.html histograms.html histograms2.html gantt.html image.html image2.html index.html iterate.html jitter.html layout.html lines_arrows.html linkedaxes.html margins.html monotonic_spline.html multiaxis.html multimsh.html multiplt.html nokey.html nonlinear1.html nonlinear3.html parallel.html param.html piecewise.html pm3dcolors.html pm3d.html pm3dgamma.html pm3d_lighting.html pointsize.html polar.html poldat.html prob2.html prob.html rainbow.html random.html rectangle.html rgba_lines.html rgbalpha.html rgb_variable.html rotate_labels.html rugplot.html running_avg.html scatter.html simple.html singulr.html smooth.html solar_path.html spline.html smooth_splines.html steps.html stringvar.html surface1.html surface2.html azimuth.html transparent.html transparent_solids.html textbox.html tics.html timedat.html ttics.html using.html varcolor.html vector.html violinplot.html world.html world2.html animate2.html heatmaps.html stats.html zerror.html
 
 index.html: index.save
 	cp index.save index.html

demo/html/Makefile.canvas
@@ -11,7 +11,7 @@ GNUPLOT_LIB = ..
 #
 html_3d: binary.html bivariat.html contours.html datastrings.html discrete.html errorbars.html fenceplot.html hidden.html hidden2.html margins.html multimsh.html pm3dcolors.html pm3d.html pm3d_lighting.html pointsize.html random.html rgb_variable.html scatter.html singulr.html surface1.html surface2.html azimuth.html transparent_solids.html vector.html world.html world2.html zerror.html
 
-html_2d: airfoil.html approximate.html array.html barchart_art.html bins.html boxplot.html boxclusters.html candlesticks.html canvas_utf8.html circles.html cities.html controls.html custom_key.html dashtypes.html electron.html enhanced_utf8.html fillbetween.html fillcrvs.html fillstyle.html finance.html histograms.html histograms2.html hypertext.html gantt.html index.html iterate.html jitter.html layout.html lines_arrows.html linkedaxes.html monotonic_spline.html multiaxis.html multiplt.html nonlinear1.html nonlinear3.html nokey.html param.html piecewise.html polar.html poldat.html prob2.html prob.html rainbow.html ellipse.html rectangle.html rgba_lines.html rgbalpha.html rotate_labels.html rugplot.html running_avg.html simple.html smooth.html solar_path.html smooth_splines.html spline.html steps.html transparent.html timedat.html ttics.html using.html varcolor.html world.html heatmaps.html stats.html violinplot.html
+html_2d: airfoil.html approximate.html array.html barchart_art.html bins.html boxplot.html boxclusters.html candlesticks.html canvas_utf8.html circles.html cities.html columnhead.html controls.html custom_key.html dashtypes.html electron.html enhanced_utf8.html fillbetween.html fillcrvs.html fillstyle.html finance.html histograms.html histograms2.html hypertext.html gantt.html index.html iterate.html jitter.html layout.html lines_arrows.html linkedaxes.html monotonic_spline.html multiaxis.html multiplt.html nonlinear1.html nonlinear3.html nokey.html param.html piecewise.html polar.html poldat.html prob2.html prob.html rainbow.html ellipse.html rectangle.html rgba_lines.html rgbalpha.html rotate_labels.html rugplot.html running_avg.html simple.html smooth.html solar_path.html smooth_splines.html spline.html steps.html transparent.html timedat.html ttics.html using.html varcolor.html world.html heatmaps.html stats.html violinplot.html
 
 html: html_2d html_3d
 

demo/html/Makefile.svg
@@ -9,7 +9,7 @@ GNUPLOT_LIB = ..
 #
 # Auto-generation of the svg demo set.
 #
-html: airfoil.html approximate.html armillary.html array.html barchart_art.html binary.html bins.html bivariat.html boxplot.html boxclusters.html candlesticks.html circles.html cities.html contours.html controls.html custom_key.html dashtypes.html datastrings.html discrete.html electron.html enhanced_utf8.html errorbars.html fenceplot.html fillbetween.html fillcrvs.html fillstyle.html finance.html hidden.html hidden2.html histograms.html histograms2.html hypertext.html gantt.html index.html iterate.html jitter.html layout.html lines_arrows.html linkedaxes.html margins.html monotonic_spline.html multiaxis.html multimsh.html multiplt.html nonlinear1.html nonlinear3.html nokey.html parallel.html param.html piecewise.html pm3dcolors.html pm3d.html pm3d_lighting.html pointsize.html polar.html poldat.html prob2.html prob.html rainbow.html random.html rectangle.html rgba_lines.html rgbalpha.html rgb_variable.html rotate_labels.html rugplot.html running_avg.html scatter.html simple.html singulr.html smooth.html smooth_splines.html spline.html solar_path.html steps.html stringvar.html surface1.html surface2.html azimuth.html textbox.html transparent.html transparent_solids.html tics.html timedat.html ttics.html using.html varcolor.html vector.html violinplot.html world.html world2.html heatmaps.html ellipse.html stats.html zerror.html
+html: airfoil.html approximate.html armillary.html array.html barchart_art.html binary.html bins.html bivariat.html boxplot.html boxclusters.html candlesticks.html circles.html cities.html columnhead.html contours.html controls.html custom_key.html dashtypes.html datastrings.html discrete.html electron.html enhanced_utf8.html errorbars.html fenceplot.html fillbetween.html fillcrvs.html fillstyle.html finance.html hidden.html hidden2.html histograms.html histograms2.html hypertext.html gantt.html index.html iterate.html jitter.html layout.html lines_arrows.html linkedaxes.html margins.html monotonic_spline.html multiaxis.html multimsh.html multiplt.html nonlinear1.html nonlinear3.html nokey.html parallel.html param.html piecewise.html pm3dcolors.html pm3d.html pm3d_lighting.html pointsize.html polar.html poldat.html prob2.html prob.html rainbow.html random.html rectangle.html rgba_lines.html rgbalpha.html rgb_variable.html rotate_labels.html rugplot.html running_avg.html scatter.html simple.html singulr.html smooth.html smooth_splines.html spline.html solar_path.html steps.html stringvar.html surface1.html surface2.html azimuth.html textbox.html transparent.html transparent_solids.html tics.html timedat.html ttics.html using.html varcolor.html vector.html violinplot.html world.html world2.html heatmaps.html ellipse.html stats.html zerror.html
 
 index.html: index.svg
 	cp index.svg index.html

demo/html/index.canvas
@@ -124,6 +124,7 @@
     <li><a href="array.html">arrays</a></li>
     <li><a href="fenceplot.html">fence plots</a></li>
     <li><a href="gantt.html">Gantt chart</a></li>
+    <li><a href="columnhead.html">points labeled by origin</a></li>
     <li><a href="heatmaps.html">2D and 3D heat maps</a></li>
     <li><a href="solar_path.html">solar path diagram</a></li>
     <li><a href="violinplot.html">violin plot</a></li>
@@ -220,7 +221,7 @@
 <tbody><tr valign="top">
 
     <td>
-    <i>This page generated March 2017</i>
+    <i>This page generated September 2017</i>
     </td>
 
     <td>

demo/html/index.save
@@ -132,6 +132,7 @@ Here are samples of PNG output from some of the demos.
     <li><a href="array.html">arrays</a></li>
     <li><a href="fenceplot.html">fence plots</a></li>
     <li><a href="gantt.html">Gantt chart</a></li>
+    <li><a href="columnhead.html">points labeled by origin</a></li>
     <li><a href="heatmaps.html">2D and 3D heat maps</a></li>
     <li><a href="solar_path.html">solar path diagram</a></li>
     <li><a href="violinplot.html">violin plot</a></li>
@@ -194,7 +195,7 @@ Here are samples of PNG output from some of the demos.
 <tbody><tr valign="top">
 
     <td>
-    <i>This page generated April 2017</i>
+    <i>This page generated September 2017</i>
     </td>
 
     <td>

demo/html/index.svg
@@ -130,6 +130,7 @@ on mouse-coordinate tracking.
     <li><a href="array.html">arrays</a></li>
     <li><a href="fenceplot.html">fence plots</a></li>
     <li><a href="gantt.html">Gantt chart</a></li>
+    <li><a href="columnhead.html">points labeled by origin</a></li>
     <li><a href="heatmaps.html">2D and 3D heat maps</a></li>
     <li><a href="solar_path.html">solar path diagram</a></li>
     <li><a href="violinplot.html">violin plot</a></li>
@@ -197,7 +198,7 @@ on mouse-coordinate tracking.
 <tbody><tr valign="top">
 
     <td>
-    <i>This page generated April 2017</i>
+    <i>This page generated September 2017</i>
     </td>
 
     <td>

src/datafile.h
@@ -1,5 +1,5 @@
 /*
- * $Id: datafile.h,v 1.53 2017/06/01 23:23:43 sfeam Exp $
+ * $Id: datafile.h,v 1.54 2017/07/20 18:04:18 sfeam Exp $
  */
 
 /* GNUPLOT - datafile.h */
@@ -130,6 +130,7 @@ extern TBOOLEAN df_warn_on_missing_columnheader;
 
 /* Used by plot title columnhead, stats name columnhead */
 extern char *df_key_title;
+extern struct at_type *df_plot_title_at;
 
 /* Prototypes of functions exported by datafile.c */
 

commit 826c9f9e670a4c4f2b485aaa69e42046ac162f91

Explicit title in plot command should override autotitle setting
E.g.
   set key autotitle columnhead
   plot FOO using 1:2 title "Special"

(bug introduced by commit e96f1b9493 "defer evaluation of plot title")

src/datafile.c
@@ -3060,11 +3060,12 @@ df_set_key_title(struct curve_points *plot)
     if (plot->title_is_suppressed)
 	return;
 
-    /* Note: I think we can only get here for histogram labels */
-    free(plot->title);
-    plot->title = df_key_title;
-    df_key_title = NULL;
-    plot->title_no_enhanced = !keyT.enhanced;
+    /* Do not replace a title that was explicitly given in the plot command */
+    if (plot->title == NULL) {
+	plot->title = df_key_title;
+	df_key_title = NULL;
+	plot->title_no_enhanced = !keyT.enhanced;
+    }
 }
 
 /*