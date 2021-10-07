76f4a3890787ff43ced571f4a45c90627d9efe72
Ethan A Merritt <merritt@u.washington.edu>
2021-10-07 15:05:25 -0700

    consistent clipping of text labels to page
    Terminal range is defined as 0 <= x < xmax;  0 <= y < ymax
    but on_page() was clipping against 0<x<xmax 0<y<ymax.
    I.e. labels at x=0 or y=0 were clipped.  Now they aren't.
    Bug #2464

--- src/term.c
+++ src/term.c
@@ -2568,7 +2568,7 @@ on_page(int x, int y)
     if (term->flags & TERM_CAN_CLIP)
 	return TRUE;
 
-    if ((0 < x && x < term->xmax) && (0 < y && y < term->ymax))
+    if ((0 <= x && x < term->xmax) && (0 <= y && y < term->ymax))
 	return TRUE;
 
     return FALSE;
