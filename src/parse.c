gnuplot/gnuplot

commit 935329b4869a860df4fbc7119ab7ec0aad33fd84

apply unary minus to constants while parsing

src/parse.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char *RCSid() { return RCSid("$Id: parse.c,v 1.74 2012/10/31 20:16:11 sfeam Exp $"); }
+static char *RCSid() { return RCSid("$Id: parse.c,v 1.75 2012/10/31 20:40:24 sfeam Exp $"); }
 #endif
 
 /* GNUPLOT - parse.c */
@@ -856,9 +856,20 @@ parse_unary_expression()
 	parse_unary_expression();
 	(void) add_action(BNOT);
     } else if (equals(c_token, "-")) {
+	struct at_entry *previous;
 	c_token++;
 	parse_unary_expression();
-	(void) add_action(UMINUS);
+	/* Collapse two operations PUSHC <pos-const> + UMINUS
+	 * into a single operation PUSHC <neg-const>
+	 */
+	previous = &(at->actions[at->a_count-1]);
+	if (previous->index == PUSHC &&  previous->arg.v_arg.type == INTGR) {
+	    previous->arg.v_arg.v.int_val = -previous->arg.v_arg.v.int_val;
+	} else if (previous->index == PUSHC &&  previous->arg.v_arg.type == CMPLX) {
+	    previous->arg.v_arg.v.cmplx_val.real = -previous->arg.v_arg.v.cmplx_val.real;
+	    previous->arg.v_arg.v.cmplx_val.imag = -previous->arg.v_arg.v.cmplx_val.imag;
+	} else
+	    (void) add_action(UMINUS);
     } else if (equals(c_token, "+")) {	/* unary + is no-op */
 	c_token++;
 	parse_unary_expression();

commit 91976ef1642b1a1e5ddaefa2a53cfcc2135d3fe7

incorrect evaluation of unary minus applied to ternary operator
Version 5.0 to present
gnuplot> show at -(a?2:3)
        push a
        jtern +3
        pushc 2
        jump +2
        pushc -3
After this fix
gnuplot> show at -(a?2:3)
        push a
        jtern +3
        pushc 2
        jump +2
        pushc 3
        uminus

This fixes an error introduced by commit 935329b4
Bug #2468

src/parse.c
@@ -1085,13 +1085,18 @@ parse_unary_expression()
 	parse_unary_expression();
 	/* Collapse two operations PUSHC <pos-const> + UMINUS
 	 * into a single operation PUSHC <neg-const>
+	 * Oct 2021: invalid if the previous constant is the else part of a conditional
 	 */
 	previous = &(at->actions[at->a_count-1]);
-	if (previous->index == PUSHC &&  previous->arg.v_arg.type == INTGR) {
-	    previous->arg.v_arg.v.int_val = -previous->arg.v_arg.v.int_val;
-	} else if (previous->index == PUSHC &&  previous->arg.v_arg.type == CMPLX) {
-	    previous->arg.v_arg.v.cmplx_val.real = -previous->arg.v_arg.v.cmplx_val.real;
-	    previous->arg.v_arg.v.cmplx_val.imag = -previous->arg.v_arg.v.cmplx_val.imag;
+	if (previous->index == PUSHC
+	&&  (at->a_count < 2 || (at->actions[at->a_count-2]).index != JUMP)) {
+	    if (previous->arg.v_arg.type == INTGR) {
+		previous->arg.v_arg.v.int_val = -previous->arg.v_arg.v.int_val;
+	    } else if (previous->arg.v_arg.type == CMPLX) {
+		previous->arg.v_arg.v.cmplx_val.real = -previous->arg.v_arg.v.cmplx_val.real;
+		previous->arg.v_arg.v.cmplx_val.imag = -previous->arg.v_arg.v.cmplx_val.imag;
+	    } else
+		(void) add_action(UMINUS);
 	} else
 	    (void) add_action(UMINUS);
     } else if (equals(c_token, "+")) {	/* unary + is no-op */