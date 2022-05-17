a8792bcee45c9a76ba7f4b5117f80e50b88c5196
Ethan A Merritt <merritt@u.washington.edu>
2022-05-17 16:52:07 -0700

    Fix:   contour binary data "with labels"
    It makes no sense to by default try to read two binary entries
    (z + label text) from a binary file for each point on the
    contoured surface.  The label will be generated automatically
    by the program; it isn't stored as a data value.
    Change the default number of binary entries per point to 1.

--- src/datafile.c
+++ src/datafile.c
@@ -3508,7 +3508,7 @@ df_bin_default_columns default_style_cols[] = {
     {XYERRORLINES, 3, 1},
     {FILLEDCURVES, 1, 1},
     {PM3DSURFACE, 1, 2},
-    {LABELPOINTS, 2, 1},
+    {LABELPOINTS, 1, 1},
     {HISTOGRAMS, 1, 0},
     {IMAGE, 1, 2},
     {RGBIMAGE, 3, 2},
@@ -5133,6 +5133,7 @@ df_readbinary(double v[], int max)
 	    this_record->memory_data = memory_data;
 
 	    FPRINTF((stderr,"Fast matrix code:\n"));
+	    FPRINTF((stderr,"\t\t %d binary columns\n", df_no_bin_cols));
 	    FPRINTF((stderr,"\t\t skip %ld bytes, read %ld bytes per point %ld total as %d x %d array\n",
 		    record_skip, bytes_per_point, bytes_total, scan_size[0], scan_size[1]));
 
