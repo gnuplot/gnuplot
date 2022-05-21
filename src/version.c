2f4b205264b8795322e8680eeaa66ce52a3bf337
Ethan A Merritt <merritt@u.washington.edu>
2022-05-21 11:52:03 -0700

    update copyright dates, version timestamp, and NEWS snapshot

--- src/version.c
+++ src/version.c
@@ -40,9 +40,9 @@ const char gnuplot_patchlevel[] = "4alpha";
 #ifdef DEVELOPMENT_VERSION
 #include "timestamp.h"
 #else
-const char gnuplot_date[] = "2022-01-12 ";
+const char gnuplot_date[] = "2022-05-21 ";
 #endif
-const char gnuplot_copyright[] = "Copyright (C) 1986-1993, 1998, 2004, 2007-2021";
+const char gnuplot_copyright[] = "Copyright (C) 1986-1993, 1998, 2004, 2007-2022";
 
 const char faq_location[] = FAQ_LOCATION;
 
