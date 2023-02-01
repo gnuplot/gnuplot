gnuplot/gnuplot

commit 912309aab1d39877e30e33984672a105d8047931

remove configure option --with-row-help
Make it a user option instead:
    set help {rows|columns}

This is clearly a per-user preference, not something that should be
a build-time option.  Why would you expect all end users to have the
same preference?

src/set.c
@@ -48,6 +48,7 @@
 #include "gp_hist.h"
 #include "gp_time.h"
 #include "gplocale.h"
+#include "help.h"
 #include "hidden3d.h"
 #include "jitter.h"
 #include "loadpath.h"
@@ -90,6 +91,7 @@ static void set_dummy(void);
 static void set_encoding(void);
 static void set_fit(void);
 static void set_grid(void);
+static void set_help(void);
 static void set_hidden3d(void);
 static void set_history(void);
 static void set_pixmap(void);
@@ -298,6 +300,9 @@ set_command()
 	    break;
 	case S_GRID:
 	    set_grid();
+	    break;
+	case S_HELP:
+	    set_help();
 	    break;
 	case S_HIDDEN3D:
 	    set_hidden3d();
@@ -2046,6 +2051,20 @@ set_grid()
     }
 }
 
+
+/* process 'set help {rows|columns}' */
+static void
+set_help()
+{
+    c_token++;
+    if (almost_equals(c_token,"col$umns"))
+	help_sort_by_rows = FALSE;
+    else if (almost_equals(c_token,"row$s"))
+	help_sort_by_rows = TRUE;
+    else
+	int_error(c_token, "unrecognized option");
+    c_token++;
+}    
 
 /* process 'set hidden3d' command */
 static void

src/show.c
@@ -47,6 +47,7 @@
 #include "fit.h"
 #include "gp_time.h"
 #include "graphics.h"
+#include "help.h"
 #include "hidden3d.h"
 #include "jitter.h"
 #include "misc.h"
@@ -101,6 +102,7 @@ static void show_style_rectangle(void);
 static void show_style_circle(void);
 static void show_style_ellipse(void);
 static void show_grid(void);
+static void show_help(void);
 static void show_raxis(void);
 static void show_paxis(void);
 static void show_zeroaxis(AXIS_INDEX);
@@ -261,6 +263,9 @@ show_command()
 	break;
     case S_GRID:
 	show_grid();
+	break;
+    case S_HELP:
+	show_help();
 	break;
     case S_RAXIS:
 	show_raxis();
@@ -1749,6 +1754,14 @@ show_grid()
 	fprintf(stderr, "\tGrid shown in spiderplots\n");
 
     fprintf(stderr, "\tGrid drawn at %s\n", (grid_layer==-1) ? "default layer" : ((grid_layer==0) ? "back" : "front"));
+}
+
+/* process 'show help' command */
+static void
+show_help()
+{
+    fprintf(stderr,"\thelp subtopics are sorted by %s\n",
+	help_sort_by_rows ? "row" : "column");
 }
 
 static void

configure.ac
@@ -752,13 +752,13 @@ if test "$with_extra_coordinate" != no; then
 fi
 
 dnl Sort help/subtopic tables by row or column
-AC_ARG_WITH(row-help,dnl
-[  --with-row-help         format help and subtopic tables by row (default)
-  --without-row-help      format help and subtopic tables by column],
-  [if test "$with_row_help" = no; then
-     AC_DEFINE(COLUMN_HELP,1,
-               [ Define if you want online help and subtopic tables sorted by column. ])
-   fi])
+dnl AC_ARG_WITH(row-help,dnl
+dnl [  --with-row-help         format help and subtopic tables by row (default)
+dnl   --without-row-help      format help and subtopic tables by column],
+dnl   [if test "$with_row_help" = no; then
+dnl      AC_DEFINE(COLUMN_HELP,1,
+dnl                [ Define if you want online help and subtopic tables sorted by column. ])
+dnl    fi])
 
 dnl Whether we want to create the LaTeX tutorial
 dnl TUTORIAL=notutorial
@@ -1443,11 +1443,11 @@ else
   AC_MSG_RESULT([  Command-line history file:                       no])
 fi
 
-if test "$with_row_help" = no; then
-  AC_MSG_RESULT([  Sort help tables by column rather than row:      yes])
-else
-  AC_MSG_RESULT([  Sort help tables by column rather than row:      no])
-fi
+dnl if test "$with_row_help" = no; then
+dnl   AC_MSG_RESULT([  Sort help tables by column rather than row:      yes])
+dnl else
+dnl   AC_MSG_RESULT([  Sort help tables by column rather than row:      no])
+dnl fi
 
 if test "$enable_mouse" = yes; then
   AC_MSG_RESULT([  Mouse support in interactive terminals:          yes])

src/help.c
@@ -49,6 +49,9 @@ void OutLine(const char *M){fputs(M,stderr);}
 # include <graph.h>
 #endif
 
+/* User control of help layout "set help {columns|rows}" */
+TBOOLEAN help_sort_by_rows = FALSE;
+
 /*
  ** help -- help subsystem that understands defined keywords
  **
@@ -573,9 +576,8 @@ ShowSubtopics(
  * the next column is skipped */
 #define COLLENGTH	18
 
-#ifndef COLUMN_HELP
-    {
-	/* sort subtopics by row - default */
+if (help_sort_by_rows) {
+	/* sort subtopics by row */
 	int subtopic;
 	int spacelen = 0, ispacelen;
 	int pos = 0;
@@ -610,9 +612,9 @@ ShowSubtopics(
 	    (void) strcat(line, "\n");
 	    OutLine_InternalPager(line);
 	}
-    }
-#else /* COLUMN_HELP */
-    {
+
+    } else {
+
 	/* sort subtopics by column */
 	int subtopic, sublen;
 	int spacelen = 0, ispacelen;
@@ -643,7 +645,6 @@ ShowSubtopics(
 	    OutLine_InternalPager(line);
 	}
     }
-#endif /* COLUMN_HELP */
 
     if (subtopics)
 	*subtopics = (subt != 0);

src/help.h
@@ -41,6 +41,9 @@
 #define	H_NOTFOUND	1	/* didn't find the keyword */
 #define	H_ERROR		(-1)	/* didn't find the help file */
 
+/* User control of help layout */
+extern TBOOLEAN help_sort_by_rows;
+
 /* Prototypes from file "help.c" */
 
 int  help(char *keyword, char *path, TBOOLEAN *subtopics);

src/tables.c
@@ -205,6 +205,7 @@ const struct gen_table set_tbl[] =
     { "fu$nction", S_FUNCTIONS },
     { "fu$nctions", S_FUNCTIONS },
     { "g$rid", S_GRID },
+    { "help", S_HELP },
     { "hid$den3d", S_HIDDEN3D },
     { "historysize", S_HISTORYSIZE },	/* Deprecated */
     { "his$tory", S_HISTORY },

src/tables.h
@@ -75,7 +75,7 @@ enum set_id {
     S_BOXDEPTH, S_BOXWIDTH, S_CLABEL, S_CLIP, S_CNTRPARAM, S_CNTRLABEL, S_CONTOUR,
     S_COLOR, S_COLORMAP, S_COLORSEQUENCE, S_CORNERPOLES,
     S_DASHTYPE, S_DATA, S_DATAFILE, S_DECIMALSIGN, S_DGRID3D, S_DUMMY, S_ENCODING,
-    S_FIT, S_FONTPATH, S_FORMAT, S_FUNCTIONS, S_GRID,
+    S_FIT, S_FONTPATH, S_FORMAT, S_FUNCTIONS, S_GRID, S_HELP,
     S_HIDDEN3D, S_HISTORY, S_HISTORYSIZE, S_ISOSAMPLES, S_ISOTROPIC, S_PIXMAP,
     S_JITTER, S_KEY, S_LABEL, S_LINK, S_NONLINEAR,
     S_LINESTYLE, S_LINETYPE, S_LOADPATH, S_LOCALE, S_LOGSCALE, S_MACROS,

commit fd157b1533510c2630d15efc030daacb0c605cd1

special case for NO_GIH
Commit 912309aa broke the build for wgnuplot.exe because it
does not include the body of help.c
The rows or columns help setting is irrelevant in this case,
so hide the "set help" and "show help" commands behind

Bug #2584

src/set.c
@@ -2056,6 +2056,7 @@ set_grid()
 static void
 set_help()
 {
+#ifndef NO_GIH
     c_token++;
     if (almost_equals(c_token,"col$umns"))
 	help_sort_by_rows = FALSE;
@@ -2064,6 +2065,7 @@ set_help()
     else
 	int_error(c_token, "unrecognized option");
     c_token++;
+#endif
 }    
 
 /* process 'set hidden3d' command */

src/show.c
@@ -1760,8 +1760,10 @@ show_grid()
 static void
 show_help()
 {
+#ifndef NO_GIH
     fprintf(stderr,"\thelp subtopics are sorted by %s\n",
 	help_sort_by_rows ? "row" : "column");
+#endif
 }
 
 static void