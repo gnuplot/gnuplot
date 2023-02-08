gnuplot/gnuplot

commit 3ff055fb11f4bf59593ed888a8bfef3a41549b21

fix variable pointtype, pointsize in plot styles yerrorlines and yerrorbars
Despite what the documentation said, these two plot styles did not
correctly track variable point properties, with the exception of
    using x:y:ylow:yhigh:(varprop) with yerrorbars
and even there the variable pointsize was lost unless pointintervalbox
was set to 0.

Now both plot styles work for either x:y:ydelta or x:y:ylow:yhigh
and either or both of pointstyle variable and pointtype variable.

It remains a limitation that "pt variable" cannot load character
point types.  This is due to field overloading in struct coordinate.

Bugfix

src/graphics.c
@@ -2400,13 +2400,9 @@ plot_points(struct curve_points *plot)
 		    && x <= plot_bounds.xright - p_width
 		    && y <= plot_bounds.ytop - p_height)) {
 
-		if ((plot->lp_properties.p_size == PTSZ_VARIABLE)
-		&&  (plot->plot_style == POINTSTYLE || plot->plot_style == LINESPOINTS
-		     || plot->plot_style == YERRORBARS))
-		    (*t->pointsize)(pointsize * plot->points[i].CRD_PTSIZE);
-
 		/* Feb 2016: variable point type */
 		if ((plot->plot_style == POINTSTYLE || plot->plot_style == LINESPOINTS
+		     || plot->plot_style == YERRORLINES
 		     || plot->plot_style == YERRORBARS)
 		&&  (plot->lp_properties.p_type == PT_VARIABLE)
 		&&  !(isnan(plot->points[i].CRD_PTTYPE))) {
@@ -2429,6 +2425,12 @@ plot_points(struct curve_points *plot)
 			term_apply_lp_properties(&(plot->lp_properties));
 		    }
 		}
+
+		if ((plot->lp_properties.p_size == PTSZ_VARIABLE)
+		&&  (plot->plot_style == POINTSTYLE || plot->plot_style == LINESPOINTS
+		     || plot->plot_style == YERRORLINES
+		     || plot->plot_style == YERRORBARS))
+		    (*t->pointsize)(pointsize * plot->points[i].CRD_PTSIZE);
 
 		/* rgb variable  -  color read from data column */
 		check_for_variable_color(plot, &plot->varcolor[i]);

src/plot2d.c
@@ -920,48 +920,34 @@ get_data(struct curve_points *current_plot)
 	}
 
 	case YERRORLINES:
-	{   /* x y ydelta   or    x y ylow yhigh */
-	    coordval ylow  = (j > 3) ? v[2] : v[1] - v[2];
-	    coordval yhigh = (j > 3) ? v[3] : v[1] + v[2];
-	    store2d_point(current_plot, i++, v[0], v[1],
-			v[0], v[0], ylow, yhigh, -1.0);
-	    break;
-	}
-
 	case YERRORBARS:
 	{   /* NB: assumes CRD_PTSIZE == xlow CRD_PTTYPE == xhigh CRD_PTCHAR == ylow
 		   lc variable, if present, was already extracted and j reduced by 1
 	     */
-	    /* x y ydelta {lc variable} */
-	    if (j == 3) {
-		coordval ylow  = v[1] - v[2];
-		coordval yhigh = v[1] + v[2];
-		store2d_point(current_plot, i++, v[0], v[1],
-			v[0], v[0], ylow, yhigh, -1.0);
-
 	    /* x y ylow yhigh {var_ps} {var_pt} {lc variable} */
-	    } else {
-		int var = 4; /* column number for next variable spec */
-		coordval ylow  = v[2];
-		coordval yhigh = v[3];
-		coordval var_pt = current_plot->lp_properties.p_type;
-		coordval var_ps = current_plot->lp_properties.p_size;
+	    coordval ylow  = v[2];
+	    coordval yhigh = v[3];
+	    coordval var_pt = current_plot->lp_properties.p_type;
+	    coordval var_ps = current_plot->lp_properties.p_size;
 
-		if (var_ps == PTSZ_VARIABLE) {
-		    if (var >= j)
-			int_error(NO_CARET, "Not enough using specs");
-		    var_ps = v[var++];
-		}
-		if (var_pt == PT_VARIABLE) {
-		    if (var >= j)
-			int_error(NO_CARET, "Not enough using specs");
-		    var_pt = v[var++];
-		}
-		if (!(var_pt > 0)) /* Catches CRD_PTCHAR (NaN) also */
-		    var_pt = 0;
-		store2d_point(current_plot, i++, v[0], v[1],
-					    var_ps, var_pt, ylow, yhigh, -1.0);
+	    if (var_pt == PT_VARIABLE) {
+		if (j < 4)
+		    int_error(NO_CARET, "Not enough using specs");
+		var_pt = v[--j];
 	    }
+	    if (!(var_pt > 0)) /* Catches CRD_PTCHAR (NaN) also */
+		var_pt = 0;
+	    if (var_ps == PTSZ_VARIABLE) {
+		if (j < 4)
+		    int_error(NO_CARET, "Not enough using specs");
+		var_ps = v[--j];
+	    }
+	    if (j == 3) {
+		ylow = v[1] - v[2];
+		yhigh = v[1] + v[2];
+	    }
+	    store2d_point(current_plot, i++, v[0], v[1],
+					var_ps, var_pt, ylow, yhigh, -1.0);
 	    break;
 	}
 
@@ -1389,8 +1375,9 @@ store2d_point(
 	cp->yhigh = yhigh;
 	break;
     case YERRORBARS:		/* auto-scale ylow yhigh */
-	cp->xlow = xlow;
-	cp->xhigh = xhigh;
+    case YERRORLINES:		/* auto-scale ylow yhigh */
+	cp->CRD_PTSIZE = xlow;
+	cp->CRD_PTTYPE = xhigh;
 	STORE_AND_UPDATE_RANGE(cp->ylow, ylow, dummy_type, current_plot->y_axis,
 				current_plot->noautoscale, cp->ylow = -VERYLARGE);
 	STORE_AND_UPDATE_RANGE(cp->yhigh, yhigh, dummy_type, current_plot->y_axis,

docs/gnuplot.doc
@@ -5062,6 +5062,7 @@ Ffigure_xerrorbars
       4 columns:  x  y  xlow  xhigh
 
  An additional input column (4th or 5th) may be used to provide variable color.
+ This style does not permit variable point properties.
 
 2 xyerrorbars
 ?plotting styles xyerrorbars
@@ -5086,8 +5087,8 @@ Ffigure_xyerrorbars
 
        plot 'data' using 1:2:($1-$3):($1+$3):4:5 with xyerrorbars
 
- An additional input column (5th or 7th) may be used to provide variable
- (per-datapoint) color information.
+ An additional input column (5th or 7th) may be used to provide variable color.
+ This style does not permit variable point properties.
 
 #TeX \newpage
 2 yerrorbars
@@ -5135,6 +5136,7 @@ Ffigure_xerrorlines
       4 columns:  x  y  xlow  xhigh
 
  An additional input column (4th or 5th) may be used to provide variable color.
+ This style does not permit variable point properties.
 
 2 xyerrorlines
 ?plotting styles xyerrorlines
@@ -5160,8 +5162,8 @@ Ffigure_xyerrorlines
 
        plot 'data' using 1:2:($1-$3):($1+$3):4:5 with xyerrorlines
 
- An additional input column (5th or 7th) may be used to provide variable
- (per-datapoint) color information.
+ An additional input column (5th or 7th) may be used to provide variable color.
+ This style does not permit variable point properties.
 
 2 yerrorlines
 ?plotting styles yerrorlines
@@ -5182,8 +5184,8 @@ Ffigure_yerrorlines
       3 columns:  x  y  ydelta
       4 columns:  x  y  ylow  yhigh
 
- An additional input column (4th or 5th) may be used to provide information
- such as variable point color.
+ Additional input columns may be used to provide information such as variable
+ point size, point type, or color.
 
  See also
 ^ <a href="http://www.gnuplot.info/demo/mgr.html">

commit c2fe2fcbe1c93c6b8c3852dc461e304c0dec7639

commit 3ff055fb broke polar mode yerrorbars
commit 3ff055fb was intended to allow variable ptsize or pttype
with 2D plot modes yerrorbars and yerrorlines.  However it failed
to take into accout polar mode plots, where variable point
properties would overwrite the error bar extents in polar coords.

This fixes the breakage at the cost of not being able to
use variable point properties for polar mode errorbars.

src/graphics.c
@@ -2417,8 +2417,8 @@ plot_points(struct curve_points *plot)
 
 		/* Feb 2016: variable point type */
 		if ((plot->plot_style == POINTSTYLE || plot->plot_style == LINESPOINTS
-		     || plot->plot_style == YERRORLINES
-		     || plot->plot_style == YERRORBARS)
+		     || (!polar && (plot->plot_style == YERRORLINES))
+		     || (!polar && (plot->plot_style == YERRORBARS)))
 		&&  (plot->lp_properties.p_type == PT_VARIABLE)
 		&&  !(isnan(plot->points[i].CRD_PTTYPE))) {
 		    pointtype = plot->points[i].CRD_PTTYPE - 1;
@@ -2443,8 +2443,8 @@ plot_points(struct curve_points *plot)
 
 		if ((plot->lp_properties.p_size == PTSZ_VARIABLE)
 		&&  (plot->plot_style == POINTSTYLE || plot->plot_style == LINESPOINTS
-		     || plot->plot_style == YERRORLINES
-		     || plot->plot_style == YERRORBARS))
+		     || (plot->plot_style == YERRORLINES && !polar)
+		     || (plot->plot_style == YERRORBARS && !polar)))
 		    (*t->pointsize)(pointsize * plot->points[i].CRD_PTSIZE);
 
 		/* rgb variable  -  color read from data column */

src/plot2d.c
@@ -946,7 +946,11 @@ get_data(struct curve_points *current_plot)
 		ylow = v[1] - v[2];
 		yhigh = v[1] + v[2];
 	    }
-	    store2d_point(current_plot, i++, v[0], v[1],
+	    if (polar) /* Polar mode bars need both xlow/high and ylow/high */
+		store2d_point(current_plot, i++, v[0], v[1],
+					v[0], v[0], ylow, yhigh, -1.0);
+	    else
+		store2d_point(current_plot, i++, v[0], v[1],
 					var_ps, var_pt, ylow, yhigh, -1.0);
 	    break;
 	}
@@ -1376,8 +1380,8 @@ store2d_point(
 	break;
     case YERRORBARS:		/* auto-scale ylow yhigh */
     case YERRORLINES:		/* auto-scale ylow yhigh */
-	cp->CRD_PTSIZE = xlow;
-	cp->CRD_PTTYPE = xhigh;
+	cp->xlow = xlow;	/* really theta if polar; CRD_PTSIZE otherwise */
+	cp->xhigh = xhigh;	/* really theta if polar; CRD_PTTYPE otherwise */
 	STORE_AND_UPDATE_RANGE(cp->ylow, ylow, dummy_type, current_plot->y_axis,
 				current_plot->noautoscale, cp->ylow = -VERYLARGE);
 	STORE_AND_UPDATE_RANGE(cp->yhigh, yhigh, dummy_type, current_plot->y_axis,