67bc1b2784136ff665fa2e20358b42bb60b79cee
Ethan A Merritt <merritt@u.washington.edu>
2023-02-10 23:05:13 -0800

    pseudofiles '+' and '++' must ignore "set datafile columnheader"
    The generated data never comes with columnheaders.
    If a previous call to "set datafile" is in effect for actual
    data files we must ignore it for any generated data.
    
    This bug was being triggered by some of the standard demos.
    For safety I am also modifying those demos to "unset datafile"
    explicitly before exiting.  Retaining the columnheader setting
    can mess up subsequent demos that don't want it, even aside
    from the issue of pseudodata.
    
    Bug #2585

--- src/datafile.c
+++ src/datafile.c
@@ -1142,6 +1142,9 @@ df_open(const char *cmd_filename, int max_using, struct curve_points *plot)
 	parse_1st_row_as_headers = TRUE;
     else
 	parse_1st_row_as_headers = FALSE;
+    /* Pseudofiles '+' and '++' can never have column headers */
+    if (df_pseudodata > 0)
+	parse_1st_row_as_headers = FALSE;
 
     if (!cmd_filename)
 	int_error(c_token, "missing filename");
