38807184b5b1c72dd66813d6293ca6ce1f864cfe
Ethan A Merritt <merritt@u.washington.edu>
2023-04-13 11:47:28 -0700

    consolidate all relevant operations into reset_datafile; call it from "reset"

--- src/plot.c
+++ src/plot.c
@@ -760,7 +760,6 @@ init_session()
 	/* Reset program variables not handled by 'reset' */
 	overflow_handling = INT64_OVERFLOW_TO_FLOAT;
 	suppress_warnings = FALSE;
-	reset_datafile();
 
 	/* Reset voxel data structures if supported */
 	init_voxelsupport();
