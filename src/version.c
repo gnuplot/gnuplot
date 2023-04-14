688086afdc3d14f272bac3dbc1be426df403bca9
Ethan A Merritt <merritt@u.washington.edu>
2023-04-13 22:38:40 -0700

    update status dates and installation guide for this snapshot
    to be updated again when -rc0 is ready to go

--- src/version.c
+++ src/version.c
@@ -40,7 +40,7 @@ const char gnuplot_patchlevel[] = "0alpha";
 #ifdef DEVELOPMENT_VERSION
 #include "timestamp.h"
 #else
-const char gnuplot_date[] = "2023-03-14 ";
+const char gnuplot_date[] = "2023-04-13 ";
 #endif
 const char gnuplot_copyright[] = "Copyright (C) 1986-1993, 1998, 2004, 2007-2023";
 
