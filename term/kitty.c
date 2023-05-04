56e0766f2b4f7daaf81df242808a8e7ca99eeb97
Andrew Rodland <andrew@cleverdomain.org>
2023-05-04 01:55:10 -0400

    Add kitty output-encoding helpers
    This gets its own .c file because it will be used from gd.trm and
    cairo.trm.
    
    These are functions that can be used together with the write_base64
    functions to send image data encoded as kitty inline images
    (https://sw.kovidgoyal.net/kitty/graphics-protocol/) with base64
    payload.

---
+++ term/kitty.c
@@ -0,0 +1,60 @@
+/*
+ * Kitty terminal support using existing PNG routines.
+ * This file is #included from term.h if either the gd or cairo
+ * libraries are present.
+ */
+
+#ifdef TERM_BODY
+
+#ifndef KITTY_C
+#define KITTY_C
+
+/* Kitty graphics spec says this must be <= 4096 */
+#define KITTY_BUFSIZ 4096
+
+#define KITTY_FORMAT_RGB 24
+#define KITTY_FORMAT_RGBA 32
+#define KITTY_FORMAT_PNG 100
+
+static unsigned char kitty_format;
+static unsigned char kitty_buf[KITTY_BUFSIZ];
+static unsigned char *kitty_writepos;
+static TBOOLEAN kitty_init_sent;
+
+static void
+kitty_init(unsigned char format)
+{
+    kitty_format = format;
+    kitty_writepos = kitty_buf;
+    kitty_init_sent = FALSE;
+}
+
+static int
+kitty_flush(TBOOLEAN more)
+{
+    /* TODO: return EOF if any writes error */
+
+    if (!kitty_init_sent) {
+        fprintf(gpoutfile, "\033_Ga=T,f=%d,m=%d;", kitty_format, more);
+        kitty_init_sent = TRUE;
+    } else {
+        fprintf(gpoutfile, "\033_Gm=%d;", more);
+    }
+    fwrite(kitty_buf, sizeof(unsigned char), kitty_writepos - kitty_buf, gpoutfile);
+    fprintf(gpoutfile, "\033\\");
+    kitty_writepos = kitty_buf;
+    return 0;
+}
+
+static int
+kitty_writechar(void *closure, unsigned char ch)
+{
+    *(kitty_writepos++) = ch;
+    if (kitty_writepos - kitty_buf >= KITTY_BUFSIZ) {
+        return kitty_flush(TRUE);
+    }
+    return 0;
+}
+
+#endif /* KITTY_C */
+#endif /* TERM_BODY */
