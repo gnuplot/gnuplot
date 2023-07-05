gnuplot/gnuplot

commit 4d02ce8b014e48eda477ae16e9ea2526c1219302

wxt: fix uninitialized toggle state for refresh of plot display

src/wxterminal/wxt_gui.cpp
@@ -3023,6 +3023,7 @@ void wxtPanel::wxt_cairo_refresh()
 	wxt_in_key_sample = FALSE;
 #ifdef USE_MOUSE
 	wxt_initialize_key_boxes(0);
+	wxt_initialize_hidden(0);
 #endif
 
 	/* Initialize the hypertext tracking mechanism */

commit 51f93f9ed94ad3de14c4d4f945e0387bf1ebbc23

commit 4d02ce8b01 broke wxt toggling; revert

src/wxterminal/wxt_gui.cpp
@@ -3023,7 +3023,6 @@ void wxtPanel::wxt_cairo_refresh()
 	wxt_in_key_sample = FALSE;
 #ifdef USE_MOUSE
 	wxt_initialize_key_boxes(0);
-	wxt_initialize_hidden(0);
 #endif
 
 	/* Initialize the hypertext tracking mechanism */