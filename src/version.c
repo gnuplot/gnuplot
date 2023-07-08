32cf2ca45fa7ed7477e8ef9174d3571aafa82bf5
Ethan A Merritt <merritt@u.washington.edu>
2023-07-08 13:47:55 -0700

    Set patchlevel to rc1

--- src/version.c
+++ src/version.c
@@ -37,11 +37,11 @@
 
 const double gnuplot_ver = 6.0;
 const char gnuplot_version[] = "6.0";
-const char gnuplot_patchlevel[] = "1";
+const char gnuplot_patchlevel[] = "rc1";
 #ifdef DEVELOPMENT_VERSION
 #include "timestamp.h"
 #else
-const char gnuplot_date[] = "2023-06-22 ";
+const char gnuplot_date[] = "2023-07-08 ";
 #endif
 const char gnuplot_copyright[] = "Copyright (C) 1986-1993, 1998, 2004, 2007-2023";
 
